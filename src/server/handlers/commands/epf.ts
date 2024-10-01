439c77397de7f4ce24398cfe2dae2124c84e99ec
nhaar <38634785+nhaar@users.noreply.github.com>
2024-10-01 12:43:55 -0300

    add awards command

--- src/server/handlers/commands/epf.ts
+++ src/server/handlers/commands/epf.ts
@@ -6,6 +6,12 @@ const handler = new XtHandler();
 handler.xt('m#sm', (client, id, message) => {
   if (message.startsWith('!epf')) {
     client.joinRoom(Room.VRRoom);
+  } else if (message.startsWith('!awards')) {
+    // grant m7-m11 awards for speedrunning
+    const awards = [815, 817, 819,822,8007]
+    awards.forEach((award) => {
+      client.addItem(award)
+    })
   }
 });
 
