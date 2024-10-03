79c4f8d8adfa6b60d8cdd772a15c90948e7ec4f4
nhaar <38634785+nhaar@users.noreply.github.com>
2024-10-03 09:31:11 -0300

    update ignore and rpc id

--- src/client/discord/constants.ts
+++ src/client/discord/constants.ts
@@ -5,5 +5,5 @@ export const SWF_ROOMS_PATH = '/rooms/';
 export const SWF_GAMES_PATH = '/games/';
 export const SWF_QUESTS_GAMES_PATH = '/games/quests/';
 export const SWF_IGLOO_ROOM_PATH = 'igloo.swf';
-export const DISCORD_RPC_CLIENT_APP_ID = '1076298709073145907';
+export const DISCORD_RPC_CLIENT_APP_ID = '1291366747684999231';
 export const LARGE_IMAGE_KEY = 'main-logo2';
