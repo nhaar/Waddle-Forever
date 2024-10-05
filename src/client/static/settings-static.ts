c3affe49fafc46d915fcd3f0b4e54fca1690d8ab
nhaar <38634785+nhaar@users.noreply.github.com>
2024-10-05 16:05:55 -0300

    add comment

--- src/client/static/settings-static.ts
+++ src/client/static/settings-static.ts
@@ -1,5 +1,7 @@
 const api = (window as any).api;
 
+// TODO better system, less anys?
+
 const fpsInput = document.querySelector<HTMLInputElement>('.js-fps-input');
 const thinIceIgtInput = document.querySelector<HTMLInputElement>('.js-thin-ice-igt-input');
 
