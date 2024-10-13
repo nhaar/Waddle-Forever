4784dff1702572cf6210590d80009658e93d33b9
nhaar <38634785+nhaar@users.noreply.github.com>
2024-10-12 22:18:06 -0300

    add all items command

--- src/server/handlers/commands/clothing.ts
+++ src/server/handlers/commands/clothing.ts
@@ -1,13 +1,18 @@
+import { items } from "../../game/item";
 import { XtHandler } from "..";
 
 const handler = new XtHandler();
 
 handler.xt('m#sm', (client, id, message) => {
   if (message.startsWith('!ai')) {
-    const numberMatch = message.match(/!ai\s+(\d+)/);
-    if (numberMatch !== null) {
-      const itemId = Number(numberMatch[1]);
-      client.addItem(itemId);
+    if (message.match(/!ai\s+all/) !== null) {
+      Object.values(items).map((item) => client.addItem(item.id));
+    } else {
+      const numberMatch = message.match(/!ai\s+(\d+)/);
+      if (numberMatch !== null) {
+        const itemId = Number(numberMatch[1]);
+        client.addItem(itemId);
+      }
     }
   }
 });
