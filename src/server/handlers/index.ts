feedc96d88577b85a3e17a3503bbdcf3ef2d07ae
nhaar <38634785+nhaar@users.noreply.github.com>
2024-10-28 21:17:13 -0300

    fix disconnect listeners being discarded

--- src/server/handlers/index.ts
+++ src/server/handlers/index.ts
@@ -72,5 +72,6 @@ export class XtHandler {
         this.listeners.set(name, [...existingCallbacks, ...callbacks]);
       }
     });
+    this.disonnectListeners = [...this.disonnectListeners, ...handler.disonnectListeners];
   }
 }
