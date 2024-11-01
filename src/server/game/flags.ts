e2f8ad83ef44f61b1e5ee9aac9cf46328ac203df
nhaar <38634785+nhaar@users.noreply.github.com>
2024-11-01 12:58:22 -0300

    update correct flags

--- src/server/game/flags.ts
+++ src/server/game/flags.ts
@@ -1,3 +1,8 @@
 export function isFlag(item: number) {
-  return item >= 500 && item <= 548
+  return (
+    (item >= 500 && item <= 548) ||
+    (item >= 7095 && item <= 7096) ||
+    (item === 7148) ||
+    (item >= 7182 && item <= 7189)
+  )
 }
