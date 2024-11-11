51ebe5ab5b67e701aeed0c8922c33e8c97409ae8
nhaar <38634785+nhaar@users.noreply.github.com>
2024-11-11 19:23:53 -0300

    use range system for organizing http routes

--- src/server/http.ts
+++ src/server/http.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 import fs from 'fs';
 import { Request, Router } from "express";
-import { SettingsManager } from "./settings";
+import { GameVersion, SettingsManager } from "./settings";
+import { isGreaterOrEqual, isLower } from './routes/versions';
 
 type GetCallback = (settings: SettingsManager, route: string) => string
 
@@ -10,6 +11,113 @@ type DirCallback = (settings: SettingsManager, dirPath: string) => string | unde
 type DataCallback = (settings: SettingsManager) => string
 
 type PathRepresentation = string | string[]
+
+type RouteMap = Array<[HttpRouter, string]>
+
+type Alternator = [GameVersion, ...string[]]
+
+type AlternatorMap = Array<[GameVersion, string]>
+
+type Spacer = [GameVersion, GameVersion, ...string[]]
+
+type SpacerMap = Array<[GameVersion, GameVersion, string]>
+
+/** Assigns files to date intervals that start when the previous ends */
+export function alternating(routers: HttpRouter[], alternators: Alternator[]) {
+  const getHandler = (map: AlternatorMap) =>  {
+    return (s: SettingsManager): string | undefined => {
+      if (isLower(s.settings.version, map[0][0])) {
+        return undefined;
+      }
+      for (let i = 0; i < map.length - 1; i++) {
+        if (isLower(s.settings.version, map[i + 1][0])) {
+          return map[i][1];
+        }
+      }
+
+      return map.slice(-1)[0][1];
+    }
+  }
+  const map = new Map<HttpRouter, AlternatorMap>()
+  for (const alternator of alternators) {
+    const targetPaths = alternator.slice(1);
+    if (routers.length !== targetPaths.length) {
+      throw new Error('Number of routers doesn\'t match number of specified targets');
+    }
+    for (let i = 0; i < targetPaths.length; i++) {
+      let currentMap = map.get(routers[i]);
+      if (currentMap === undefined) {
+        currentMap = []
+      }
+      currentMap.push([alternator[0], targetPaths[i]]);
+      map.set(routers[i], currentMap);
+    }
+  }
+
+  map.forEach((value, key) => {
+    if (key.file) {
+      key.get('', getHandler(value))
+    } else {
+      key.dir('', getHandler(value))
+    }
+  })
+}
+
+/** Assigns files in a date intervals in which you can leave space out between them */
+export function spaced(routers: HttpRouter[], spacers: Spacer[]) {
+  const getHandler = (map: SpacerMap) =>  {
+    return (s: SettingsManager): string | undefined => {
+      for (const interval of map) {
+        if (isLower(s.settings.version, interval[0])) {
+          return undefined
+        }
+        if (isLower(s.settings.version, interval[1])) {
+          return interval[2];
+        }
+      }
+      
+      return undefined;
+    }
+  }
+  const map = new Map<HttpRouter, SpacerMap>()
+  for (const spacer of spacers) {
+    const targetPaths = spacer.slice(2);
+    if (routers.length !== targetPaths.length) {
+      throw new Error('Number of routers doesn\'t match number of specified targets');
+    }
+    for (let i = 0; i < targetPaths.length; i++) {
+      let currentMap = map.get(routers[i]);
+      if (currentMap === undefined) {
+        currentMap = []
+      }
+      currentMap.push([spacer[0], spacer[1], targetPaths[i]]);
+      map.set(routers[i], currentMap);
+    }
+  }
+
+  map.forEach((value, key) => {
+    if (key.file) {
+      key.get('', getHandler(value))
+    } else {
+      key.dir('', getHandler(value))
+    }
+  })
+}
+
+/** Assigns files to routers in a given date interval */
+export function range(start: GameVersion, end: GameVersion, routeMaps: RouteMap) {
+  const routers: HttpRouter[] = [];
+  const targets: string[] = [];
+
+  for (const routeMap of routeMaps) {
+    routers.push(routeMap[0]);
+    targets.push(routeMap[1]);
+  }
+  
+  spaced(routers, [
+    [start, end, ...targets]
+  ])
+}
 
 function processPathRepresentation(repr: PathRepresentation): string[] {
   if (typeof repr === 'string') {
@@ -26,10 +134,12 @@ function getExpressRoute(route: string[]): string {
 export class HttpRouter {
   parent: HttpServer | HttpRouter
   route: string[]
+  file: boolean
 
-  constructor(route: PathRepresentation, parent: HttpServer | HttpRouter) {
+  constructor(route: PathRepresentation, parent: HttpServer | HttpRouter, file: boolean = false) {
     this.parent = parent;
     this.route = processPathRepresentation(route);
+    this.file = file
   }
 
   get (route: PathRepresentation, handler: GetCallback) {
@@ -38,6 +148,10 @@ export class HttpRouter {
 
   dir (route: PathRepresentation, handler: DirCallback) {
     this.parent.dir([...this.route, ...processPathRepresentation(route)], handler);
+  }
+
+  hash (): string {
+    return this.parent.hash() + '/' + this.route.join('/')
   }
 }
 
@@ -71,7 +185,6 @@ export class HttpServer {
   dir (route: PathRepresentation, handler: DirCallback) {
     this.router.get(`${getExpressRoute(processPathRepresentation(route))}*`, (req: Request, res, next) => {
       const dirPath = req.params[0];
-      console.log(dirPath)
       const specialPath = handler(this.settingsManager, dirPath);
       if (specialPath === undefined) {
         next();
@@ -84,5 +197,9 @@ export class HttpServer {
         }
       }
     })
+  }
+
+  hash (): string {
+    return ''
   }
 }
--- src/server/routes/game.ts
+++ src/server/routes/game.ts
@@ -1,6 +1,5 @@
-import { HttpRouter, HttpServer } from "../http";
+import { alternating, HttpRouter, HttpServer, range, spaced } from "../http";
 import { SettingsManager } from "../settings";
-import { isGreaterOrEqual, isLower, isLowerOrEqual } from './versions';
 import { getStampbook } from './stampjson';
 
 export function createHttpServer(settingsManager: SettingsManager): HttpServer {
@@ -26,12 +25,58 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
 
   const rooms = new HttpRouter('rooms', globalContent);
 
+  const stage = new HttpRouter('stage.swf', rooms);
+
+  const plaza = new HttpRouter('plaza.swf', rooms);
+
+  const rink = new HttpRouter('rink.swf', rooms);
+
+  const globalCrumbs = new HttpRouter(['crumbs', 'global_crumbs.swf'], globalContent);
+
+  const map = new HttpRouter('map.swf', globalContentContent);
+
+  const binoculars = new HttpRouter('binoculars', globalContent);
+
+  const iglooBackground = new HttpRouter(['igloo', 'assets', 'igloo_background.swf'], globalContent);
+
+  const scavengerHunt = new HttpRouter('scavenger_hunt', globalContent);
+
+  const login = new HttpRouter('login', localContentEn);
+
   const localEnCatalogues = new HttpRouter('catalogues', localContentEn);
 
   const enCloseUps = new HttpRouter('close_ups', localContentEn);
 
   const enNews = new HttpRouter('news', localContentEn);
+
+  const worldAchievements = new HttpRouter(['web_service', 'worldachievements.xml'], server);
+
+  const emptyTelescope = new HttpRouter('empty.swf', telescope);
+
+  const iglooMusic = new HttpRouter('igloo_music.swf', globalContentContent);
+
+  const prizebooth = new HttpRouter('prizebooth.swf', localEnCatalogues);
+
+  const prizeboothmember = new HttpRouter('prizeboothmember.swf', localEnCatalogues);
+
+  const costume = new HttpRouter('costume.swf', localEnCatalogues);
+
+  const sportCatalogue = new HttpRouter('sport.swf', localEnCatalogues);
   
+  const furnitureCatalogue = new HttpRouter('furniture.swf', localEnCatalogues);
+
+  const clothingCatalogue = new HttpRouter('clothing.swf', localEnCatalogues);
+
+  const iglooCatalogue = new HttpRouter('igloo.swf', localEnCatalogues);
+
+  const petFurnitureCatalogue = new HttpRouter('pet.swf', localEnCatalogues);
+
+  const ninjaCatalogue = new HttpRouter('ninja.swf', localEnCatalogues);
+
+  const newCrumbs = new HttpRouter('news_crumbs.swf', enNews);
+
+  const library = new HttpRouter(['forms', 'library.swf'], localContentEn);
+
   // TODO a better system for handling these special medias
   // entrypoint for as2 client
   server.get('boots.swf', (s) => {
@@ -67,103 +112,85 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
     return `versions/paddle/white.swf`;
   });
 
-  rooms.get('stage.swf', (s) => {
-    if (isGreaterOrEqual(s.settings.version, '2010-Sep-03') && isLower(s.settings.version, '2010-Sep-24')) {
-      return `versions/stage/squidzoid/2009_10/stage.swf`
-    } else if (isLower(s.settings.version, '2010-Oct-23')) {
-      return `versions/stage/fairy/stage.swf`
-    } else if (isLower(s.settings.version, '2010-Nov-24')) {
-      return `versions/stage/bamboo/stage.swf`
-    } else {
-      return `versions/stage/planety/stage.swf`
-    }
+  // HALLOWEEN PARTY 2010
+  range('2010-Oct-28', '2010-Nov-24',
+    [
+      [rooms, 'versions/2010/halloween/rooms'],
+      [rink, 'versions/2010/halloween/rooms/rink.swf'],
+      [map, 'versions/2010/halloween/map.swf'],
+      [telescope, 'versions/2010/halloween/telescope'],
+      [globalCrumbs, 'versions/2010/halloween/global_crumbs.swf'],
+      [binoculars, 'versions/2010/halloween/binoculars'],
+      [iglooBackground, 'versions/2010/halloween/igloo_background.swf'],
+      [scavengerHunt, 'versions/2010/halloween/scavenger_hunt'],
+      [localEnCatalogues, 'versions/2010/halloween/catalogues'],
+      [login, 'versions/2010/halloween/login'],
+      [client, 'versions/2010/halloween/client'],
+      [worldAchievements, 'versions/2010/halloween/worldachievements.xml']
+    ]
+  )
 
-    throw new Error('Not implemented');
-  })
+  // FAIR 2010
+  range('2010-Sep-03', '2010-Sep-24',[
+    [globalCrumbs, 'versions/2010/fair/global_crumbs.swf'],
+    [rooms, 'versions/2010/fair/rooms'],
+    [login, 'versions/2010/fair/login'],
+    [client, 'versions/2010/fair/client'],
+    [worldAchievements, 'versions/2010/fair/worldachievements.xml']
+  ]);
 
-  rooms.get('plaza.swf', (s) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03':
-      case '2010-Sep-10':
-        return `versions/2010/fair/rooms/plaza.swf`
-      case '2010-Oct-28': return `versions/2010/halloween/rooms/plaza.swf`;
-    }
+  // FAIR 2010 first half
+  range('2010-Sep-03', '2010-Sep-10', [
+    [prizebooth, 'versions/2010/fair/start/prizebooth.swf'],
+    [prizeboothmember, 'versions/2010/fair/start/prizeboothmember.swf']
+  ])
 
-    if (isGreaterOrEqual(s.settings.version, '2010-Sep-24') && isLower(s.settings.version, '2010-Oct-23')) {
-      return `versions/stage/fairy/plaza.swf`
-    } else if (isLower(s.settings.version, '2010-Nov-24')) {
-      return `versions/stage/bamboo/plaza.swf`
-    } else {
-      return `versions/stage/planety/plaza.swf`
-    }
+  // FAIR 2010 second half
+  range('2010-Sep-10', '2010-Sep-24', [
+    [prizebooth, 'versions/2010/fair/end/prizebooth.swf'],
+    [prizeboothmember, 'versions/2010/fair/end/prizeboothmember.swf']
+  ])
 
-    throw new Error('Not implemented');
-  })
+  // 5th ANNIVERSARY
+  range('2010-Oct-23', '2010-Oct-28', [
+    [rooms, 'versions/2010/anniversary/rooms'],
+    [globalCrumbs, 'versions/2010/anniversary/global_crumbs.swf']
+  ]);
 
-  rooms.get('rink.swf', (s) => {    
-    switch (s.settings.version) {
-      case '2010-Oct-28': return `versions/2010/halloween/rooms/rink.swf`;
-    }
+  alternating(
+    [stage],
+    [
+      ['2010-Sep-03', 'versions/stage/squidzoid/2009_10/stage.swf'],
+      ['2010-Sep-24', 'versions/stage/fairy/stage.swf'],
+      ['2010-Oct-23', 'versions/stage/bamboo/stage.swf'],
+      ['2010-Nov-24', 'versions/stage/planety/stage.swf']
+    ]
+  );
 
-    if (isLower(s.settings.version, '2010-Sep-24')) {
-      return `versions/stadium/2010_05/rink.swf`
-    } else {
-      return `versions/2010/stadium_games/rink.swf`;
-    }
+  alternating(
+    [rink],
+    [
+      ['2010-Sep-03', 'versions/stadium/2010_05/rink.swf'],
+      ['2010-Sep-24', 'versions/2010/stadium_games/rink.swf']
+    ]
+  );
 
-    throw new Error('Not implemented');
-  })
+  range('2010-Oct-23', '2010-Oct-28', [
+    [emptyTelescope, 'versions/telescope/storm_on_horizon.swf']
+  ])
 
-  rooms.dir('', (s, d) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03':
-      case '2010-Sep-10':
-        return `versions/2010/fair/rooms`;
-      case '2010-Oct-23': return `versions/2010/anniversary/rooms`;
-      case '2010-Oct-28': return `versions/2010/halloween/rooms`;
-      default: return undefined;
-    }
-  })
+  alternating([iglooMusic],
+    [
+      ['2010-Sep-03', 'versions/igloo/2010_08_20/igloo_music.swf'],
+      ['2010-Nov-24', 'versions/igloo/2010_11_12/igloo_music.swf']
+    ]
+  )
 
-  telescope.get('empty.swf', (s) => {
-    if (s.settings.version === '2010-Oct-23') {
-      return `versions/telescope/storm_on_horizon.swf`;
-    }
-  })
-
-  telescope.dir('', (s, d) => {
-    switch (s.settings.version) {
-      case '2010-Oct-28': return `versions/2010/halloween/telescope`
-      default: return undefined;
-    }
-  })
-
-  globalContentContent.get('igloo_music.swf', (s) => {
-    let date = ''
-    if (isLower(s.settings.version, '2010-Nov-24')) {
-      date = '2010_08_20';
-    } else if (s.settings.version === '2010-Nov-24') {
-      date = '2010_11_12';
-    } else {
-      date = '2011_05_13'
-    }
-
-    return `versions/igloo/${date}/igloo_music.swf`;
-  })
-
-  globalContentContent.get('map.swf', (s) => {
-    switch (s.settings.version) {
-      case '2010-Oct-28': return 'versions/2010/halloween/map.swf';
-    }
-
-    if (isLower(s.settings.version, '2010-Sep-24')) {
-      return undefined;
-    } else {
-      return `versions/map/2010_09_24.swf`;
-    }
-
-    throw new Error('Not implemented');
-  })
+  alternating([map],
+    [
+      ['2010-Sep-24', 'versions/map/2010_09_24.swf']
+    ]
+  );
 
   globalContent.get('tickets.swf', () => {
     return `versions/2010/fair/tickets.swf`
@@ -173,17 +200,9 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
     return `versions/2010/fair/ticket_icon.swf`
   })
 
-  globalContent.get(['crumbs', 'global_crumbs.swf'], (s) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03':
-      case '2010-Sep-10':
-        return `versions/2010/fair/global_crumbs.swf`
-      case '2010-Sep-24': return `versions/2010/stadium_games/global_crumbs.swf`;
-      case '2010-Oct-23': return `versions/2010/anniversary/global_crumbs.swf`;
-      case '2010-Oct-28': return `versions/2010/halloween/global_crumbs.swf`;
-      default: return undefined;
-    }
-  })
+  spaced([globalCrumbs], [
+    ['2010-Sep-24', '2010-Oct-23', 'versions/2010/stadium_games/global_crumbs.swf']
+  ]);
 
   globalContent.dir('clothing', (s) => {
     return s.settings.clothing ? 'clothing' : undefined;
@@ -193,140 +212,60 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
     return 'music'
   })
 
-  globalContent.dir('binoculars', (s) => {
-    switch (s.settings.version) {
-      case '2010-Oct-28': return `versions/2010/halloween/binoculars`;
-      default: return undefined;
-    }
-  })
-
-  globalContent.get(['igloo', 'assets', 'igloo_background.swf'], (s) => {
-    switch (s.settings.version) {
-      case '2010-Oct-28': return 'versions/2010/halloween/igloo_background.swf';
-      default: undefined;
-    }
-  })
-
-  globalContent.dir('scavenger_hunt', (s) => {
-    switch (s.settings.version) {
-      case '2010-Oct-28': return `versions/2010/halloween/scavenger_hunt`;
-      default: return undefined;
-    }
-  })
-
   globalContent.dir('furniture', () => {
     return `furniture`
   })
 
-  localEnCatalogues.get('prizebooth.swf', (s) => {
-    if (s.settings.version === '2010-Sep-03') {
-      return `versions/2010/fair/start/prizebooth.swf`
-    } else if (s.settings.version === '2010-Sep-10') {
-      return `versions/2010/fair/end/prizebooth.swf`
-    }
-  })
+  // STAGE PLAYS
+  alternating(
+    [plaza, costume],
+    [
+      ['2010-Sep-03', '', 'versions/stage/squidzoid/2011_03/costume.swf'],
+      ['2010-Sep-24', 'versions/stage/fairy/plaza.swf', 'versions/stage/fairy/costume.swf'],
+      ['2010-Oct-23', 'versions/stage/bamboo/plaza.swf', 'versions/stage/bamboo/costume.swf'],
+      ['2010-Nov-24', 'versions/stage/planety/plaza.swf', 'versions/stage/planety/costume.swf']
+    ]
+  )
 
-  localEnCatalogues.get('prizeboothmember.swf', (s) => {
-    if (s.settings.version === '2010-Sep-03') {
-      return `versions/2010/fair/start/prizeboothmember.swf`
-    } else if (s.settings.version === '2010-Sep-10') {
-      return `versions/2010/fair/end/prizeboothmember.swf`
-    }
-  })
+  alternating([sportCatalogue], [
+    ['2010-Sep-03', 'versions/stadium/2010_05/sport.swf'],
+    ['2010-Sep-24', 'versions/2010/stadium_games/sport.swf']
+  ])
 
-  localEnCatalogues.get('costume.swf', (s) => {
-    if (isGreaterOrEqual(s.settings.version, '2010-Sep-03') && isLower(s.settings.version, '2010-Sep-24')) {
-      return `versions/stage/squidzoid/2011_03/costume.swf`
-    } else if (isLower(s.settings.version, '2010-Oct-23')) {
-      return `versions/stage/fairy/costume.swf`
-    } else if (isLower(s.settings.version, '2010-Nov-24')) {
-      return `versions/stage/bamboo/costume.swf`
-    } else {
-      return `versions/stage/planety/costume.swf`
-    }
+  alternating([furnitureCatalogue], [
+    ['2010-Sep-03', 'versions/igloo/2010_08_20/furniture.swf'],
+    ['2010-Sep-24', 'versions/igloo/2010_09_24/furniture.swf'],
+    ['2010-Oct-23', 'versions/igloo/2010_10_15/furniture.swf'],
+    ['2010-Nov-24', 'versions/igloo/2010_11_12/furniture.swf']
+  ])
 
-    throw new Error('Not implemented');
-  })
+  alternating([clothingCatalogue], [
+    ['2010-Sep-03', 'versions/clothing/2010_09_03.swf'],
+    ['2010-Oct-23', 'versions/clothing/2010_10_1.swf'],
+    ['2010-Nov-24', 'versions/clothing/2010_11_05.swf']
+  ])
 
-  localEnCatalogues.get('sport.swf', (s) => {
-    if (isLower(s.settings.version, '2010-Sep-24')) {
-      return `versions/stadium/2010_05/sport.swf`;
-    } else {
-      return `versions/2010/stadium_games/sport.swf`;
-    }
-  })
+  alternating([iglooCatalogue], [
+    ['2010-Sep-03', 'versions/igloo/2010_08_20/igloo.swf'],
+    ['2010-Nov-24', 'versions/igloo/2010_11_12/igloo.swf']
+  ])
 
-  localEnCatalogues.dir('', (s) => {
-    switch (s.settings.version) {
-      case '2010-Oct-28': return `versions/2010/halloween/catalogues`;
-      default: return undefined;
-    }
-  })
+  alternating([petFurnitureCatalogue], [
+    ['2010-Sep-03', 'versions/puffle/2010_03_19/pets.swf']
+  ])
 
-  localEnCatalogues.get('furniture.swf', (s) => {
-    let date = ''
-    if (isLower(s.settings.version, '2010-Sep-24')) {
-      date = '2010_08_20'
-    } else if (isLower(s.settings.version, '2010-Oct-23')) {
-      date = '2010_09_24'
-    } else if (isLower(s.settings.version, '2010-Nov-24')) {
-      date = '2010_10_15'
-    } else {
-      date = '2010_11_12'
-    }
-    return `versions/igloo/${date}/furniture.swf`;
-  })
+  alternating([ninjaCatalogue], [
+    ['2010-Sep-03', 'versions/ninja/2009_11_13/ninja.swf']
+  ])
 
-  localEnCatalogues.get('clothing.swf', (s) => {
-    let date = ''
-    if (isGreaterOrEqual(s.settings.version, '2010-Sep-03') && isLower(s.settings.version, '2010-Oct-23')) {
-      date = '2010_09_03'
-    } else if (isLower(s.settings.version, '2010-Nov-24')) {
-      date = '2010_10_01'
-    } else {
-      date = '2010_11_05'
-    }
-    return `versions/clothing/${date}.swf`
-  })
-
-  localEnCatalogues.get('igloo.swf', (s) => {
-    let date = ''
-    if (isLower(s.settings.version, '2010-Nov-24')) {
-      date = '2010_08_20';
-    } else {
-      date = '2010_11_12';
-    }
-
-    return `versions/igloo/${date}/igloo.swf`;
-  })
-
-  localEnCatalogues.get('pets.swf', (s) => {
-    if (isGreaterOrEqual(s.settings.version, '2010-Sep-03') || isLowerOrEqual(s.settings.version, '2010-Nov-24')) {
-      return `versions/puffle/2010_03_19/pets.swf`
-    }
-
-    throw new Error('Not implemented');
-  })
-
-  localEnCatalogues.get('ninja.swf', (s) => {
-    if (isGreaterOrEqual(s.settings.version, '2010-Sep-03') || isLowerOrEqual(s.settings.version, '2010-Nov-24')) {
-      return `versions/ninja/2009_11_13/ninja.swf`
-    }
-
-    throw new Error('Not implemented');
-  })
-
-  enNews.get('news_crumbs.swf', (s) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03': return 'versions/news_crumbs/2010_09_02.swf'
-      case '2010-Sep-10': return 'versions/news_crumbs/2010_09_09.swf'
-      case '2010-Sep-24': return 'versions/news_crumbs/2010_09_23.swf';
-      case '2010-Oct-23': return 'versions/news_crumbs/2010_10_21.swf';
-      case '2010-Oct-28': return 'versions/news_crumbs/2010_10_28.swf';
-      case '2010-Nov-24': return 'versions/news_crumbs/2010_09_30.swf';
-      default: throw new Error('Not implemented');
-    }
-  })
+  alternating([newCrumbs], [
+    ['2010-Sep-03', 'versions/news_crumbs/2010_09_02.swf'],
+    ['2010-Sep-10', 'versions/news_crumbs/2010_09_09.swf'],
+    ['2010-Sep-24', 'versions/news_crumbs/2010_09_23.swf'],
+    ['2010-Oct-23', 'versions/news_crumbs/2010_10_21.swf'],
+    ['2010-Oct-28', 'versions/news_crumbs/2010_10_28.swf'],
+    ['2010-Nov-24', 'versions/news_crumbs/2010_09_30.swf']
+  ])
 
   enNews.dir('', () => {
     return 'newspapers';
@@ -344,49 +283,16 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
     return 'versions/2010/halloween/poster.swf'
   })
 
-  localContentEn.dir('login', (s) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03':
-      case '2010-Sep-10':
-        return 'versions/2010/fair/login'
-      case '2010-Oct-28': return 'versions/2010/halloween/login'
-      default: return undefined;
-    }
-  })
-
-  localContentEn.get(['forms', 'library.swf'], (s) => {
-    if (isLower(s.settings.version, '2010-Oct-23')) {
-      return `versions/library/2009_10_24.swf`
-    } else {
-      return `versions/library/2010_10_23.swf`
-    }
-  })
+  alternating([library], [
+    ['2010-Sep-03', 'versions/library/2009_10_24.swf'],
+    ['2010-Oct-23', 'versions/library/2010_10_23.swf']
+  ])
 
   client.get('shell.swf', (s) => {
     return `special/shell/${s.settings.remove_idle ? 'no_idle' : 'vanilla'}.swf`
   })
-
-  client.dir('', (s) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03':
-      case '2010-Sep-10':
-        return 'versions/2010/fair/client';
-      case '2010-Oct-28': return 'versions/2010/halloween/client';
-      default: return undefined;
-    }
-  })
   
   server.get('', () => `special/index.html`);
-
-  server.get(['web_service', 'worldachievements.xml'], (s) => {
-    switch (s.settings.version) {
-      case '2010-Sep-03':
-      case '2010-Sep-10':
-        return 'versions/2010/fair/worldachievements.xml';
-      case '2010-Oct-28': return 'versions/2010/halloween/worldachievements.xml';
-      default: return undefined;
-    }
-  })
 
   return server
 }
