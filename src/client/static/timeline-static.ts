bd59332945c98d7bee2ae9de4087c8d94690e80a
nhaar <38634785+nhaar@users.noreply.github.com>
2025-04-02 09:16:58 -0300

    add music jam construction with no fireworks

--- src/client/static/timeline-static.ts
+++ src/client/static/timeline-static.ts
@@ -393,6 +393,12 @@ const versions: Version[] = [
     }
   },
   {
+    value: '2010-07-05',
+    events: {
+      partyUpdate: 'July 4th Fireworks Removed'
+    }
+  },
+  {
     value: '2010-08-12',
     events: {
       partyStart: 'Mountain Expedition'
