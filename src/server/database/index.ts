761e43ad1f8e86607b2329b52678382fed8b83f6
nhaar <38634785+nhaar@users.noreply.github.com>
2025-04-03 21:01:39 -0300

    update version

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -79,6 +79,8 @@ class JsonDatabase {
         return '0.3.0';
       case '0.3.0':
         return '0.3.1';
+      case '0.3.1':
+        return '0.3.2';
       default:
         throw new Error('Invalid database version: ' + version);
     }
