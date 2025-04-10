d13ad7da08dd597d20a08690c000be3ba9e63787
nhaar <38634785+nhaar@users.noreply.github.com>
2025-04-10 11:48:23 -0300

    add spy drills

--- src/server/handlers/play/game.ts
+++ src/server/handlers/play/game.ts
@@ -1,3 +1,4 @@
+import { randomInt } from "../../../common/utils";
 import { Handler } from "..";
 
 const handler = new Handler();
@@ -12,6 +13,26 @@ handler.xt('z', 'ggd', (client) => {
 handler.xt('z', 'sgd', (client, data) => {
   client.penguin.setGameData(Buffer.from(data));
   client.update();
+})
+
+// this is seemingly the same endpoint used in dance contest
+// there may be conflict, but this is for spy drills
+handler.xt('z', 'zr', (client) => {
+  // array containing 1...10
+  const games = [...Array(10).keys()].map((i) => i + 1);
+
+  // pick 3 games randomly
+  const picks = [];
+  const totalPicks = 3;
+  for (let i = 0; i < totalPicks; i++) {
+    const pick = randomInt(0, games.length - 1);
+    picks.push(...games.splice(pick, 1));
+  }
+
+  // TODO look into original algorithm
+  const medalsReward = 1;
+
+  client.sendXt('zr', picks.join(','), medalsReward);
 })
 
 export default handler;
