dbc745495427ec3ae22874e5ba0621a780a021d9
nhaar <38634785+nhaar@users.noreply.github.com>
2025-04-16 09:37:45 -0300

    refactor global crumbs script to depend on a base

---
+++ crumbs/base-crumbs.ts
@@ -0,0 +1,220 @@
+import { PARTIES, Party } from "../src/server/game/parties";
+import { isEqual, isGreaterOrEqual, isLower, Version } from "../src/server/routes/versions";
+
+export type SeasonalCrumbsChange<Modifications> = {
+  type: 'seasonal',
+  /** Valid version string */
+  version: string
+  changes: Modifications
+};
+
+export type PartyCrumbsChange<Modifications> = {
+  type: 'party',
+  /** Path to event eg 2010/music_jam */
+  event: string
+  changes: Modifications
+  start: Version
+  end: Version
+};
+
+export type CrumbsUpdate<Modifications> = SeasonalCrumbsChange<Modifications> | PartyCrumbsChange<Modifications>;
+
+export type SimpleSeasonalCrumbsUpdate<Modifications> = Omit<SeasonalCrumbsChange<Modifications>, 'type'>;
+
+export type TimelineMap<Modifications> = Map<Version, SimpleSeasonalCrumbsUpdate<Modifications>>;
+
+export type CrumbsTimeline<Modifications> = Array<SimpleSeasonalCrumbsUpdate<Modifications>>;
+
+export function getTimelineMap<Modifications>(timeline: CrumbsTimeline<Modifications>): TimelineMap<Modifications> {
+  const map = new Map<string, SimpleSeasonalCrumbsUpdate<Modifications>>();
+  timeline.forEach((value) => {
+    map.set(value.version, value);
+  });
+  return map;
+}
+
+export function getCrumbsTimelineFromMap<Modifications>(map: TimelineMap<Modifications>): CrumbsTimeline<Modifications> {
+  const timeline: CrumbsTimeline<Modifications> = [];
+  map.forEach((value) => {
+    timeline.push(value);
+  });
+
+  const sorted = timeline.sort((a, b) => {
+    const aVersion = a.version;
+    const bVersion = b.version;
+    if (isLower(aVersion, bVersion)) {
+      return -1;
+    } else if (isEqual(aVersion, bVersion)) {
+      return 0;
+    } else {
+      return 1;
+    }
+  });
+
+  return sorted;
+}
+
+export function addParties<Modifications>(
+  timeline: CrumbsTimeline<Modifications>,
+  extractPartyChanges: (party: Party) => Modifications
+): CrumbsUpdate<Modifications>[] {
+  let currentPartyIndex = 0;
+  const crumbsUpdates: CrumbsUpdate<Modifications>[] = [];
+  // go through each update in the timeline to and see if a party comes after or before it
+  for (let timelineIndex = 0; timelineIndex < timeline.length; timelineIndex++) {
+    const seasonal = timeline[timelineIndex];
+    // start at the first party we haven't done yet
+    // the `true` is no problem since we are breaking eventually
+    for (let partyIndex = currentPartyIndex; true; partyIndex++) {
+      const party = PARTIES[partyIndex];
+      // party comes after the version, we can push this seasonal update and check next version
+      // also, if partyIndex > PARTIES.length, we just add every new update
+      // we use start >= version so that if they start on the same day, the seasonal one comes first
+      if (partyIndex >= PARTIES.length || isGreaterOrEqual(party.start, seasonal.version)) {
+        crumbsUpdates.push({
+          type: 'seasonal',
+          ...seasonal
+        });
+        // exit the party index without incrementing, return to timeline loop
+        break;
+      } else {
+        const mainPath = typeof party.paths === 'string' ? party.paths : party.paths[0];
+        let end = party.end;
+        if (end === null) {
+          end = PARTIES[partyIndex + 1].start;
+        }
+
+        // check if crumbs actually changes in the day
+        const hasChanges = [party.music, party.globalPaths].some((value) => value !== undefined);
+        if (hasChanges) {
+          crumbsUpdates.push({
+            type: 'party',
+            changes: extractPartyChanges(party),
+            event: mainPath,
+            start: party.start,
+            end
+          });
+        }
+        // increment so we never have to do this party again
+        currentPartyIndex++;
+      }
+    }
+  }
+  return crumbsUpdates;
+}
+
+export function addToTimelineMap<Modifications>(
+  map: TimelineMap<Modifications>,
+  update: SimpleSeasonalCrumbsUpdate<Modifications>,
+  mergerFunction: (change1: Modifications, change2: Modifications) => Modifications
+): void {
+  const previousValue = map.get(update.version);
+  if (previousValue === undefined) {
+    map.set(update.version, update);
+  } else {
+    map.set(update.version, {
+      version: update.version,
+      changes: mergerFunction(previousValue.changes, update.changes)
+    });
+  }
+}
+
+/**
+ * This function handles putting every other module together to generate all the crumb files
+ * @param loadBaseCrumbs This must be a function that extracts the P-Code as a string
+ * @param applyChanges This is a function that must take the P-Code and a modification object, and generate new P-Code
+ * @param getFullTimeline This is a function that must get the full timeline of updates of the crumbs
+ * @param createSeasonalCrumb This is a function that has to take the P-Code and date and generate the seasonal SWF file
+ */
+export async function generateCrumbFiles<Modifications>(
+  loadBaseCrumbs: () => Promise<string>,
+  applyChanges: (content: string, changes: Modifications) =>string,
+  getFullTimeline: () => CrumbsUpdate<Modifications>[],
+  createSeasonalCrumb: (content: string, date: Version) => Promise<void>
+): Promise<void> {
+  console.log('Beginning exporting');
+  // when we are in a party, we keep it in paralel the changes with and without party
+  let nonPartyCrumbs = await loadBaseCrumbs();
+  let inPartyCrumbs = '';
+  // when null, no party is currently happening
+  let currentPartyEndDate: string | null = null;
+
+  // saving what the crumbs were before, so that we can prevent duplicating crumbs 
+  // this more specifically stores the file name, not just the date (eg multiple dates can be here)
+  let lastNonPartySeasonalDate = '';
+
+  // maps: start date -> end date
+  // every entry in this map, we will delete the end date file
+  // and add it to the start date
+  // const merges = new Map<string, string>();
+  const seasonalCrumbs = new Map<string, string>();
+
+  currentPartyEndDate = null;
+  inPartyCrumbs = '';
+
+  // function to be run when a party ends, which handles adding a crumb to the end of the last party
+  const partyEnded = () => {
+    // if it's null, then there is nothing to be done here as the party end was handled already
+    if (currentPartyEndDate === null) {
+      return;
+    }
+    // if the party ended, we update our info
+    // and we add a seasonal crumb for the party ending
+    // we do this to preserve all information update
+    // in the party
+
+    const beforePartyCrumbs = seasonalCrumbs.get(lastNonPartySeasonalDate);
+    // if the crumbs didn't change during the party, we don't add it
+    // and we will rename what we added earlier
+    // aditionally, if beforePartyCrumbs is undefined it means that this entry was deleted
+    // so we can't rename what was added
+    if (nonPartyCrumbs === beforePartyCrumbs) {
+      // "rename"
+      seasonalCrumbs.delete(lastNonPartySeasonalDate);
+      lastNonPartySeasonalDate = `${lastNonPartySeasonalDate},${currentPartyEndDate}`;
+      seasonalCrumbs.set(lastNonPartySeasonalDate, beforePartyCrumbs);
+    } else {
+      seasonalCrumbs.set(currentPartyEndDate, nonPartyCrumbs);
+    }
+  }
+
+  const crumbsUpdates = getFullTimeline();
+  for (const update of crumbsUpdates) {
+    if (update.type === 'seasonal') {
+      console.log(`Exporting changes for day ${update.version}`);
+      // check if in-party
+      if (currentPartyEndDate !== null) {
+        // check if the party ended
+        if (isGreaterOrEqual(update.version, currentPartyEndDate)) {
+          partyEnded();
+        } else {
+          // if still in party, just update the party crumbs
+          inPartyCrumbs = applyChanges(inPartyCrumbs, update.changes);
+        }
+      } else {
+        // if not in party, we save this date for reference
+        lastNonPartySeasonalDate = update.version;
+      }
+      // we always update the non-party, but
+      // we only server it if not in a party
+      nonPartyCrumbs = applyChanges(nonPartyCrumbs, update.changes);
+
+      // these files need to be waited so that we can potentially rename them
+      seasonalCrumbs.set(update.version, currentPartyEndDate === null ? nonPartyCrumbs : inPartyCrumbs);
+    } else {
+      partyEnded();
+      console.log(`Exporting changes for event "${update.event}"`);
+
+      // if there is a seasonal which started on this day, delete its entry since it will be overwritten
+      seasonalCrumbs.delete(update.start);
+
+      currentPartyEndDate = update.end;
+      inPartyCrumbs = applyChanges(nonPartyCrumbs, update.changes);
+      createSeasonalCrumb(inPartyCrumbs, update.start);
+    }
+  }
+
+  seasonalCrumbs.forEach((crumbContent, fileName) => {
+    createSeasonalCrumb(crumbContent, fileName);
+  })
+}
--- crumbs/global-crumbs.ts
+++ crumbs/global-crumbs.ts
@@ -8,16 +8,23 @@
 import path from 'path'
 import { extractPcode, replacePcode } from '../src/common/ffdec/ffdec';
 import { DEFAULT_DIRECTORY } from '../src/common/utils'
-import { PARTIES } from '../src/server/game/parties'
+import { PARTIES, Party } from '../src/server/game/parties'
 import { STAGE_PLAYS, STAGE_TIMELINE } from '../src/server/game/stage-plays'
 import { ITEMS } from '../src/server/game/items'
-import { Version, isEqual, isGreaterOrEqual, isLower } from '../src/server/routes/versions';
+import { Version, isGreaterOrEqual } from '../src/server/routes/versions';
 import { MusicUpdate, PathsUpdate, PricesUpdate } from '../src/server/game/crumbs';
+import { CrumbsUpdate, TimelineMap, CrumbsTimeline, generateCrumbFiles, getTimelineMap, getCrumbsTimelineFromMap, addToTimelineMap, addParties } from './base-crumbs';
 
 const BASE_GLOBAL_CRUMBS = path.join(__dirname, 'base_global_crumbs.swf');
 
 async function loadBaseCrumbs(): Promise<string> {
   return await extractPcode(BASE_GLOBAL_CRUMBS, 'frame_1/DoAction.pcode');
+}
+
+type GlobalModifications = {
+  music?: MusicUpdate,
+  prices?: Record<number, number>,
+  globalPaths?: PathsUpdate
 }
 
 function changeRoomMusic(crumbs: string, roomName: string, newMusicId: number): string {
@@ -87,13 +94,7 @@ function addGlobalPath(crumbs: string, pathName: string, path: string): string {
   return lines.join('\n');
 }
 
-type Modifications = {
-  music?: MusicUpdate,
-  prices?: Record<number, number>,
-  globalPaths?: PathsUpdate
-}
-
-function applyChanges(crumbs: string, changes: Modifications): string {
+function applyChanges(crumbs: string, changes: GlobalModifications): string {
   let newCrumbs = crumbs;
   if (changes.music !== undefined) {
     Object.entries(changes.music).forEach((pair) => {
@@ -117,111 +118,16 @@ function applyChanges(crumbs: string, changes: Modifications): string {
   return newCrumbs;
 }
 
-type SeasonalCrumbsChange = {
-  type: 'seasonal',
-  /** Valid version string */
-  version: string
-  changes: Modifications
-};
-type PartyCrumbsChange = {
-  type: 'party',
-  /** Path to event eg 2010/music_jam */
-  event: string
-  changes: Modifications
-  start: Version
-  end: Version
-};
+type GlobalCrumbsUpdate = CrumbsUpdate<GlobalModifications>;
 
-type CrumbsUpdate = SeasonalCrumbsChange | PartyCrumbsChange
+type GlobalTimelineMap = TimelineMap<GlobalModifications>;
 
-type SimpleSeasonalCrumbsUpdate = Omit<SeasonalCrumbsChange, 'type'>;
-
-type TimelineMap = Map<Version, SimpleSeasonalCrumbsUpdate>;
-
-type CrumbsTimeline = Array<SimpleSeasonalCrumbsUpdate>;
+type GlobalCrumbsTimeline = CrumbsTimeline<GlobalModifications>;
 
 /** Timeline with all manual updates to global_crumbs, must be included in CHRONOLOGICAL ORDER! */
-const CRUMBS_TIMELINE: CrumbsTimeline = [];
+const GLOBAL_CRUMBS_TIMELINE: GlobalCrumbsTimeline = [];
 
-function getTimelineMap(timeline: CrumbsTimeline): TimelineMap {
-  const map = new Map<string, SimpleSeasonalCrumbsUpdate>();
-  timeline.forEach((value) => {
-    map.set(value.version, value);
-  });
-  return map;
-}
-
-function getCrumbsTimelineFromMap(map: TimelineMap): CrumbsTimeline {
-  const timeline: CrumbsTimeline = [];
-  map.forEach((value) => {
-    timeline.push(value);
-  });
-
-  const sorted = timeline.sort((a, b) => {
-    const aVersion = a.version;
-    const bVersion = b.version;
-    if (isLower(aVersion, bVersion)) {
-      return -1;
-    } else if (isEqual(aVersion, bVersion)) {
-      return 0;
-    } else {
-      return 1;
-    }
-  });
-
-  return sorted;
-}
-
-function addParties(timeline: CrumbsTimeline): CrumbsUpdate[] {
-  let currentPartyIndex = 0;
-  const crumbsUpdates: CrumbsUpdate[] = [];
-  // go through each update in the timeline to and see if a party comes after or before it
-  for (let timelineIndex = 0; timelineIndex < timeline.length; timelineIndex++) {
-    const seasonal = timeline[timelineIndex];
-    // start at the first party we haven't done yet
-    // the `true` is no problem since we are breaking eventually
-    for (let partyIndex = currentPartyIndex; true; partyIndex++) {
-      const party = PARTIES[partyIndex];
-      // party comes after the version, we can push this seasonal update and check next version
-      // also, if partyIndex > PARTIES.length, we just add every new update
-      // we use start >= version so that if they start on the same day, the seasonal one comes first
-      if (partyIndex >= PARTIES.length || isGreaterOrEqual(party.start, seasonal.version)) {
-        crumbsUpdates.push({
-          type: 'seasonal',
-          ...seasonal
-        });
-        // exit the party index without incrementing, return to timeline loop
-        break;
-      } else {
-        const mainPath = typeof party.paths === 'string' ? party.paths : party.paths[0];
-        let end = party.end;
-        if (end === null) {
-          end = PARTIES[partyIndex + 1].start;
-        }
-
-        // check if crumbs actually changes in the day
-        const hasChanges = [party.music, party.globalPaths].some((value) => value !== undefined);
-        if (hasChanges) {
-          crumbsUpdates.push({
-            type: 'party',
-            changes: {
-              music: party.music,
-              globalPaths: party.globalPaths
-            },
-            event: mainPath,
-            start: party.start,
-            end
-          });
-        }
-        // increment so we never have to do this party again
-        currentPartyIndex++;
-      }
-    }
-  }
-  return crumbsUpdates;
-}
-
-function addStagePlays(map: TimelineMap): TimelineMap {
+function addStagePlays(map: GlobalTimelineMap): GlobalTimelineMap {
   // name -> music id
   const stageMap = new Map<string, number>();
   STAGE_PLAYS.forEach((play) => {
@@ -240,29 +146,21 @@ function addStagePlays(map: TimelineMap): TimelineMap {
           'stage': song
         }
       }
-    });
+    }, mergerFunction);
   })
 
   return map;
 }
 
-function addToTimelineMap(map: TimelineMap, update: SimpleSeasonalCrumbsUpdate): void {
-  const previousValue = map.get(update.version);
-  if (previousValue === undefined) {
-    map.set(update.version, update);
-  } else {
-    map.set(update.version, {
-      version: update.version,
-      changes: {
-        music: { ...previousValue.changes.music, ...update.changes.music },
-        globalPaths: { ...previousValue.changes.globalPaths, ...update.changes.globalPaths },
-        prices: { ...previousValue.changes.prices, ...update.changes.prices }
-      }
-    });
-  }
+function mergerFunction(change1: GlobalModifications, change2: GlobalModifications): GlobalModifications {
+  return {
+    music: { ...change1.music, ...change2.music },
+    globalPaths: { ...change1.globalPaths, ...change2.globalPaths },
+    prices: { ...change1.prices, ...change2.prices }
+  };
 }
 
-function addPriceChanges(map: TimelineMap): TimelineMap {
+function addPriceChanges(map: GlobalTimelineMap): GlobalTimelineMap {
   const itemRows = ITEMS.rows;
   const priceChanges: Record<Version, PricesUpdate> = {};
   itemRows.forEach((item) => {
@@ -284,17 +182,24 @@ function addPriceChanges(map: TimelineMap): TimelineMap {
       changes: {
         prices: priceChanges[date]
       }
-    });
+    }, mergerFunction);
   }
 
   return map;
 }
 
-function getFullTimeline(): CrumbsUpdate[] {
-  let map = getTimelineMap(CRUMBS_TIMELINE);
+function extractPartyChanges(party: Party): GlobalModifications {
+  return {
+    music: party.music,
+    globalPaths: party.globalPaths
+  }
+}
+
+function getFullTimeline(): GlobalCrumbsUpdate[] {
+  let map = getTimelineMap<GlobalModifications>(GLOBAL_CRUMBS_TIMELINE);
   map = addPriceChanges(map);
   map == addStagePlays(map);
-  const updates = addParties(getCrumbsTimelineFromMap(map));
+  const updates = addParties(getCrumbsTimelineFromMap<GlobalModifications>(map), extractPartyChanges);
   return updates;
 }
 
@@ -315,89 +220,10 @@ async function createSeasonalCrumb(content: string, date: Version): Promise<void
 }
 
 (async () => {
-  console.log('Beginning exporting');
-  // when we are in a party, we keep it in paralel the changes with and without party
-  let nonPartyCrumbs = await loadBaseCrumbs();
-  let inPartyCrumbs = '';
-  // when null, no party is currently happening
-  let currentPartyEndDate: string | null = null;
-
-  // saving what the crumbs were before, so that we can prevent duplicating crumbs 
-  // this more specifically stores the file name, not just the date (eg multiple dates can be here)
-  let lastNonPartySeasonalDate = '';
-
-  // maps: start date -> end date
-  // every entry in this map, we will delete the end date file
-  // and add it to the start date
-  // const merges = new Map<string, string>();
-  const seasonalCrumbs = new Map<string, string>();
-
-  currentPartyEndDate = null;
-  inPartyCrumbs = '';
-
-  // function to be run when a party ends, which handles adding a crumb to the end of the last party
-  const partyEnded = () => {
-    // if it's null, then there is nothing to be done here as the party end was handled already
-    if (currentPartyEndDate === null) {
-      return;
-    }
-    // if the party ended, we update our info
-    // and we add a seasonal crumb for the party ending
-    // we do this to preserve all information update
-    // in the party
-
-    const beforePartyCrumbs = seasonalCrumbs.get(lastNonPartySeasonalDate);
-    // if the crumbs didn't change during the party, we don't add it
-    // and we will rename what we added earlier
-    // aditionally, if beforePartyCrumbs is undefined it means that this entry was deleted
-    // so we can't rename what was added
-    if (nonPartyCrumbs === beforePartyCrumbs) {
-      // "rename"
-      seasonalCrumbs.delete(lastNonPartySeasonalDate);
-      lastNonPartySeasonalDate = `${lastNonPartySeasonalDate},${currentPartyEndDate}`;
-      seasonalCrumbs.set(lastNonPartySeasonalDate, beforePartyCrumbs);
-    } else {
-      seasonalCrumbs.set(currentPartyEndDate, nonPartyCrumbs);
-    }
-  }
-
-  const crumbsUpdates = getFullTimeline();
-  for (const update of crumbsUpdates) {
-    if (update.type === 'seasonal') {
-      console.log(`Exporting changes for day ${update.version}`);
-      // check if in-party
-      if (currentPartyEndDate !== null) {
-        // check if the party ended
-        if (isGreaterOrEqual(update.version, currentPartyEndDate)) {
-          partyEnded();
-        } else {
-          // if still in party, just update the party crumbs
-          inPartyCrumbs = applyChanges(inPartyCrumbs, update.changes);
-        }
-      } else {
-        // if not in party, we save this date for reference
-        lastNonPartySeasonalDate = update.version;
-      }
-      // we always update the non-party, but
-      // we only server it if not in a party
-      nonPartyCrumbs = applyChanges(nonPartyCrumbs, update.changes);
-
-      // these files need to be waited so that we can potentially rename them
-      seasonalCrumbs.set(update.version, currentPartyEndDate === null ? nonPartyCrumbs : inPartyCrumbs);
-    } else {
-      partyEnded();
-      console.log(`Exporting changes for event "${update.event}"`);
-
-      // if there is a seasonal which started on this day, delete its entry since it will be overwritten
-      seasonalCrumbs.delete(update.start);
-
-      currentPartyEndDate = update.end;
-      inPartyCrumbs = applyChanges(nonPartyCrumbs, update.changes);
-      createSeasonalCrumb(inPartyCrumbs, update.start);
-    }
-  }
-
-  seasonalCrumbs.forEach((crumbContent, fileName) => {
-    createSeasonalCrumb(crumbContent, fileName);
-  })
+  await generateCrumbFiles<GlobalModifications>(
+    loadBaseCrumbs,
+    applyChanges,
+    getFullTimeline,
+    createSeasonalCrumb
+  );
 })();
