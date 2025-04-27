a076c985449402def828ed0fd36593222408a0c5
nhaar <38634785+nhaar@users.noreply.github.com>
2025-04-26 22:17:12 -0300

    add cpip igloo lists

--- src/server/game/igloo-lists.ts
+++ src/server/game/igloo-lists.ts
@@ -31,7 +31,102 @@ type ListUpdate = {
   fileId?: number;
 }
 
-export const IGLOO_LISTS: [FullList, ...ListUpdate[]] = [
+export const IGLOO_LISTS: [FullList, ...Array<FullList | ListUpdate>] = [
+  // {
+  //   date: '2008-07-18',
+  //   list: [
+  //     [{ display: 'Fiesta', id: 229, new: true }, { display: 'Jazz', id: 211 }],
+  //     [{ display: 'Aqua Grabber', id: 114, new: true }, { display: 'Ocean Voyage', id: 212 }],
+  //     [{ display: 'Medieval Town', id: 233, new: true }, { display: 'Cool Surf', id: 214 }],
+  //     [{ display: 'Pizza Parlor', id: 20 }, { display: 'Coffee Shop', id: 1 }],
+  //     [{ display: 'Superhero', id: 32 }, { display: 'Dance Mix 1', id: 2 }],
+  //     [{ display: 'Fall Fair', id: 215 }, { display: 'Dance Mix 2', id: 5 }],
+  //     [{ display: 'Folk Guitar', id: 209 }, { display: 'Water Party', id: 217 }]
+  //   ]
+  // },
+  {
+    date: '2008-10-17',
+    list: [
+      [{ display: 'Halloween', id: 223, new: true }, { display: 'Pizza Parlor', id: 20 }],
+      [{ display: 'Halloween Dance', id: 224, new: true }, { display: 'Ocean Voyage', id: 212 }],
+      [{ display: 'Epic Battle', id: 236, new: true }, { display: 'Cool Surf', id: 214 }],
+      [{ display: 'Aqua Grabber', id: 114 }, { display: 'Coffee Shop', id: 1 }],
+      [{ display: 'Medieval Town', id: 233 }, { display: 'Superhero', id: 32 }],
+      [{ display: 'Fiesta', id: 229 }, { display: 'Dance Mix', id: 5 }],
+      [{ display: 'Fall Fair', id: 221 }, { display: 'Water Party', id: 217 }]
+    ]
+  },
+  {
+    date: '2008-12-12',
+    list: [
+      [{ display: 'Snowy Holiday', id: 227, new: true }, { display: 'Cool Surf', id: 214 }],
+      [{ display: 'Christmas Piano Medley', id: 255, new: true }, { display: 'Ocean Voyage', id: 212 }],
+      [{ display: 'Anniversary Party', id: 250, new: true }, { display: 'Medieval Town', id: 233 }],
+      [{ display: 'Pop Song', id: 243 }, { display: 'Epic Battle', id: 236 }],
+      [{ display: 'Team Blue 2', id: 36 }, { display: 'Superhero', id: 32 }],
+      [{ display: 'Fiesta', id: 229 }, { display: 'Aqua Grabber', id: 114 }],
+      [{ display: 'Fall Fair', id: 221 }, { display: 'Water Party', id: 217 }]
+    ]
+  },
+  {
+    date: '2009-03-22',
+    songs: [
+      { display: 'Noir Noises', id: 37, pos: [1, 1] },
+      { display: 'Egyptian Wrap', id: 34, pos: [2, 1] },
+      { display: 'For Great Justice', id: 32, pos: [5, 2] },
+      { display: 'Twice upon a Time', id: 39, pos: [6, 2] },
+      { display: 'Pterodactyl Ptune', id: 35, pos: [7, 2] }
+    ]
+  },
+  {
+    date: '2009-05-29',
+    songs: [
+      { display: 'Flipper Stomper', id: 244, pos: [3, 1] },
+      { display: 'Coconut', id: 215, pos: [5, 1] },
+      { display: 'Catchin\' Waves Theme', id: 113, pos: [6, 1] },
+      { display: 'Puffle Ragtime', id: 261, pos: [7, 1] },
+      { display: 'Summer Song', id: 216, pos: [1, 2] },
+      { display: 'Mix Maestro', id: 242, pos: [6, 2] },
+      { display: 'Water Kongo', id: 217, pos: [7, 2] }
+    ]
+  },
+  {
+    date: '2009-06-26',
+    songs: [
+      { display: 'Viking Opera', id: 41, pos: [1, 1] },
+      { display: 'Mountain Dojo', id: 21, pos: [2, 1] },
+      { display: 'Silly to Funky', id: 258, pos: [5, 1] },
+      { display: 'Planet Y', id: 38, pos: [5, 2] }
+    ]
+  },
+  {
+    date: '2009-07-31',
+    songs: [
+      { display: 'Keytar Jam', id: 275, pos: [1, 1] },
+      { display: 'Rocksteady', id: 276, pos: [1, 2] },
+      { display: 'All-Access Pass', id: 272, pos: [3, 2] },
+      { display: 'Rocking Pizza', id: 271, pos: [5, 2] }
+    ]
+  },
+  {
+    date: '2009-09-21',
+    songs: [
+      { display: 'Campfire Song', id: 220, pos: [2, 1] },
+      { display: 'Extra Anchovies', id: 270, pos: [7, 1] },
+      { display: 'All the Fun of The Fair', id: 221, pos: [2, 2] },
+      { display: 'Spicy Salsa', id: 229, pos: [6, 2] }
+    ]
+  },
+  {
+    date: '2009-10-16',
+    songs: [
+      { display: 'Team Power', id: 33, pos: [1, 1] },
+      { display: 'Ruby\'s Theme', id: 37, pos: [5, 1] },
+      { display: 'Haunted Disco', id: 223, pos: [3, 2] },
+      { display: 'Zero Gravity', id: 279, pos: [4, 2] }
+    ]
+  },
+  // date for november's is unknown
   {
     date: '2009-12-25',
     list: [
@@ -140,7 +235,7 @@ function getIglooList(date: Version): IglooList {
   const [fullList, ...updates] = IGLOO_LISTS;
 
 
-  const list = JSON.parse(JSON.stringify(fullList.list)) as IglooList;
+  let list = JSON.parse(JSON.stringify(fullList.list)) as IglooList;
 
   for (const update of updates) {
     // end on the first update that wasn't released
@@ -148,7 +243,11 @@ function getIglooList(date: Version): IglooList {
       break;
     }
 
-    updateList(list, update);
+    if ('songs' in update) {
+      updateList(list, update);
+    } else {
+      list = JSON.parse(JSON.stringify(update.list)) as IglooList;
+    }
   }
 
   return list;
