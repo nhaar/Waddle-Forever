14f427398b51df2ff133d147c1ec6d7cd4f053a8
nhaar <38634785+nhaar@users.noreply.github.com>
2025-05-05 13:41:15 -0300

    remove dead labels code

--- labels/expand-paths.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import { LABEL_FILES, LabelFile } from "./labels";
-
-Promise.all(LABEL_FILES.map((file) => new Promise<void>(async (resolve) => {
-  const labelFile = await LabelFile.fromFile(path.join(__dirname, 'groups', file));
-  labelFile.write();
-  resolve();
-})));
