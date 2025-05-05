e225be6e92280b5203f4c561cea73d3d0133d7aa
nhaar <38634785+nhaar@users.noreply.github.com>
2025-05-05 14:37:52 -0300

    automatically add version file to the media packages to make it easier to download the media itself

--- scripts/zip-media.ts
+++ scripts/zip-media.ts
@@ -4,8 +4,7 @@ import { zip, OUT_DIR, createOut } from './zip';
 import { generateGlobalCrumbs } from '../crumbs/global-crumbs'
 import { generateLocalCrumbs } from '../crumbs/local-crumbs'
 import { generateNewsCrumbsFiles } from '../crumbs/news-crumbs'
-
-const VERSION = require('../package.json').version;
+import { VERSION } from '../src/common/version';
 
 const MEDIA_DIR = path.join(__dirname, '..', 'media');
 
@@ -20,11 +19,14 @@ createOut();
   await generateLocalCrumbs();
   console.log('news crumbs');
   await generateNewsCrumbsFiles(true);
-  dirs.forEach((dir) => {
+  for (const dir of dirs) {
     const fullDir = path.join(MEDIA_DIR, dir)
+    const versionFilePath = path.join(fullDir, '.version');
+    fs.writeFileSync(versionFilePath, VERSION);
     if (fs.lstatSync(fullDir).isDirectory()) {
-      zip(fullDir, path.join(OUT_DIR, `${dir}-${VERSION}.zip`))
+      await zip(fullDir, path.join(OUT_DIR, `${dir}-${VERSION}.zip`))
     }
-  })
+    fs.unlinkSync(versionFilePath);
+  }
 })();
 
