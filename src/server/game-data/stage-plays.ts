11509ca8d774d8d3186b0148556c612adcc110a2
ungato23 <211165838+ungato23@users.noreply.github.com>
2025-10-27 18:58:36 -0300

    Add Puffle Party 2011 (#54)
    * Add Puffle Party 2011
    
    * change timeline end date
    
    * update viking opera feb 11 stage listing
    
    ---------
    
    Co-authored-by: nhaar <38634785+nhaar@users.noreply.github.com>

--- src/server/game-data/stage-plays.ts
+++ src/server/game-data/stage-plays.ts
@@ -354,5 +354,12 @@ export const STAGE_TIMELINE: Array<{
     costumeTrunkFileRef: 'archives:July09Costume.swf', // From 2009, unknown if accurate
     plazaFileRef: 'recreation:plaza_ruby_no_weather.swf',
     stageFileRef: 'archives:RoomsStage-December2010.swf' // accurate
+  },
+  {
+    date: Update.STAGE_FEB_11,
+    name: 'The Haunting of the Viking Opera',
+    costumeTrunkFileRef: 'archives:February2011HauntingOfTheVikingOperaCostumeTrunk.swf',
+    plazaFileRef: null,
+    stageFileRef: 'archives:RoomsStage-February2011.swf'
   }
 ];
