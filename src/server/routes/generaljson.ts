a9e7d96ddabdbefb5eb4ce96951957016b43b97e
nhaar <38634785+nhaar@users.noreply.github.com>
2025-11-04 10:39:09 -0300

    add migrator visits in general.json

--- src/server/routes/generaljson.ts
+++ src/server/routes/generaljson.ts
@@ -2,10 +2,12 @@
 
 import { findInVersion } from "../game-data";
 import { getMapNoteTimeline } from "../timelines/map-note";
+import { getMigratorTimeline } from "../timelines/migrator";
 import { Version } from "./versions";
 
 const mapNoteTimeline = getMapNoteTimeline();
+const migratorTimeline = getMigratorTimeline();
 
 export function getGeneralJson(version: Version): string {
-  return JSON.stringify({ "mascot_options": { "migrator_active": false }, "party_options": { "fair_ticket_active": false, "hunt_active": false, "itemRewardID": 1388, "isMapNoteActive": findInVersion(version, mapNoteTimeline), "showPartyAnnouncement": false, "party_icon_active": false }, "igloo_options": { "contestRunning": false }, "oops_test": { "testEnabled": true }, "island_options": { "isDaytime": true }, "party_dates": { "20170201": "2017-01-30" } })
+  return JSON.stringify({ "mascot_options": { "migrator_active": findInVersion(version, migratorTimeline) }, "party_options": { "fair_ticket_active": false, "hunt_active": false, "itemRewardID": 1388, "isMapNoteActive": findInVersion(version, mapNoteTimeline), "showPartyAnnouncement": false, "party_icon_active": false }, "igloo_options": { "contestRunning": false }, "oops_test": { "testEnabled": true }, "island_options": { "isDaytime": true }, "party_dates": { "20170201": "2017-01-30" } })
 }
