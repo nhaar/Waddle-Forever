1966a802615bf999d7a78fa0e739d3850f5f867d
nhaar <38634785+nhaar@users.noreply.github.com>
2025-11-26 11:18:57 -0300

    fix ghost scavenger hunt icon

--- src/server/game-data/files.ts
+++ src/server/game-data/files.ts
@@ -1713,7 +1713,7 @@ map to work with CPIP, it's used as a placeholder pre dojo courtyard`
     },
     {
       file: 'ghosts/party.swf',
-      comment: 'Alledgedly originally the Halloween Party 2014 party.swf, with possible modifications, and in addition to that, modifications done by cutestlesbian for the ghost hunter transform to work. After that, added a fix for the ghost hunter not being applying when logging in. Removed a forced JSON utils export because it was breaking stamps somehow'
+      comment: 'Alledgedly originally the Halloween Party 2014 party.swf, with possible modifications, and in addition to that, modifications done by cutestlesbian for the ghost hunter transform to work. After that, added a fix for the ghost hunter not being applying when logging in. Removed a forced JSON utils export because it was breaking stamps somehow. NOTE: the way party icon is setup is important as well, it might break the ghost scavenger hunt icon otherwise, if this file is to be replaced'
     }
   ]
 };
