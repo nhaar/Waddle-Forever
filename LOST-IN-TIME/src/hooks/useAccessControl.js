5c85a9f092c4f0a5f3700fd9f70f4c9f38a770eb
Valverde <leonidas.valverde@tecsup.edu.pe>
2025-11-27 20:30:54 -0500

    koko

--- LOST-IN-TIME/src/hooks/useAccessControl.js
+++
@@ -1,7 +0,0 @@
-import { useAccessControlContext } from '../context/AccessControlContext';
-
-export function useAccessControl() {
-  return useAccessControlContext();
-}
-
-export default useAccessControl;
