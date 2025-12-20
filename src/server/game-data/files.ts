c0e343f38c6c2d2798c45ae3c9a4992fa4becb45
nhaar <38634785+nhaar@users.noreply.github.com>
2025-12-20 12:55:18 -0300

    update penguin style september 05 recreation

--- src/server/game-data/files.ts
+++ src/server/game-data/files.ts
@@ -1807,7 +1807,7 @@ Unknown if its teleporting to village functions would be accurate`
     },
     {
       file: 'style_september_05.swf',
-      comment: 'Originally a recreation provided by v0rtex, listed as September05Style.swf in the archives wiki. Then, fixed the Hockey Stick ID which was the pin instead of the hand item'
+      comment: 'Made by Doubleuman'
     },
     {
       file: 'mtn_lucky_coin_pin.swf',
