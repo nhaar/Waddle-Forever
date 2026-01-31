da8373a3a39fa553fa78b05dd7b781453801f776
ChrisCPI <92183829+ChrisCPI@users.noreply.github.com>
2026-01-31 09:58:08 -0500

    Add UI for creating a mod from path, also menu tweaks (#151)

--- src/client/static/mods-static.ts
+++ src/client/static/mods-static.ts
@@ -32,7 +32,7 @@ function setupPage() {
   
     document.querySelector('.mods')!.innerHTML = html;
   
-    const inputs = document.querySelectorAll('input');
+    const inputs = document.querySelectorAll('input[type="checkbox"]');
     for (const input of inputs) {
       if (input instanceof HTMLInputElement) {
         input.addEventListener('change', (e) => {
@@ -47,15 +47,85 @@ function setupPage() {
         })
       }
     }
-  
-    document.getElementById('open-mods-folder')?.addEventListener('click', () => {
-      modsApi.openModsFolder();
-    });
-  
-    document.getElementById('update-mods')?.addEventListener('click', () => {
-      setupPage();
-    });
   })
 }
+
+// so that it can be called from the preload
+(window as any).setupPage = setupPage;
+
+document.getElementById('open-mods-folder')?.addEventListener('click', () => {
+  modsApi.openModsFolder();
+});
+
+document.getElementById('update-mods')?.addEventListener('click', () => {
+  setupPage();
+});
+
+const createModPrompt = document.getElementById('mods-clipboard-prompt')!;
+
+document.getElementById('mod-from-clipboard')?.addEventListener('click', () => {
+  createModPrompt.style.visibility = 'visible';
+});
+
+const createModNameInput = document.getElementById('create-mod-name-input')! as HTMLInputElement;
+const createModPathInput = document.getElementById('create-mod-path-input')! as HTMLInputElement;
+
+const createModButton = document.getElementById('create-mod-btn')!;
+
+function sendMakeMod() {
+  if (createModNameInput.value.length > 0) {
+    modsApi.makeModFromPath(createModNameInput.value, createModPathInput.value);
+    createModPrompt.style.visibility = 'hidden';
+    createModNameInput.value = '';
+    createModPathInput.value = '';
+  } else {
+    alert('Please enter a name for the mod.')
+  }
+}
+
+createModButton.addEventListener('click', () => sendMakeMod());
+
+createModNameInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') createModPathInput.focus();
+})
+
+createModPathInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') sendMakeMod();
+})
+
+createModPathInput.addEventListener('input', () => {
+  let text = createModPathInput.value;
+
+  if (text.startsWith("http://") || text.startsWith("https://")) {
+    try {
+      text = new URL(text).pathname;
+    } catch {
+      return;
+    }
+  }
+
+  const parts = text.split('/').filter(Boolean);
+
+  // don't include any file names
+  if (parts.length > 0 && /\.[^\/]+$/.test(parts[parts.length - 1])) {
+    parts.pop();
+  }
+
+  let normalized = parts.join('/');
+
+  // let slashes be typed normally
+  if (text.endsWith('/') && text.length > 1) {
+    normalized += '/';
+  }
+  if (normalized !== createModPathInput.value) {
+    const pos = createModPathInput.selectionStart;
+    createModPathInput.value = normalized;
+    createModPathInput.selectionStart = createModPathInput.selectionEnd = pos;
+  }
+
+  if (normalized !== createModPathInput.value) {
+    createModPathInput.value = normalized;
+  }
+});
 
 setupPage();
