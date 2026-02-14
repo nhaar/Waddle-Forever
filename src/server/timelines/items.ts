b80814ee1e94370c5f24b8ad4a2b04a48acf5740
nhaar <38634785+nhaar@users.noreply.github.com>
2026-02-14 20:42:25 -0300

    track item releases for snow and sport and elite gear

--- src/server/timelines/items.ts
+++ src/server/timelines/items.ts
@@ -3,13 +3,23 @@ import { UPDATES } from "../updates/updates";
 
 export const ITEM_RELEASES: Map<number, Version> = new Map<number, Version>();
 
+function addItemReleases(date: Version, items: number[]) {
+  items.forEach(item => {
+    const entry = ITEM_RELEASES.get(item);
+    if (entry === undefined) {
+      ITEM_RELEASES.set(item, date);
+    }
+  });
+}
+
 UPDATES.forEach(update => {
   if (update.update.clothingCatalog !== undefined) {
-    update.update.clothingCatalog.newItems.forEach(item => {
-      const entry = ITEM_RELEASES.get(item);
-      if (entry === undefined) {
-        ITEM_RELEASES.set(item, update.date);
-      }
-    });
+    addItemReleases(update.date, update.update.clothingCatalog.newItems);
+  }
+  if (update.update.sportCatalog !== undefined) {
+    addItemReleases(update.date, update.update.sportCatalog.items);
+  }
+  if (update.update.eliteGearItems !== undefined) {
+    addItemReleases(update.date, update.update.eliteGearItems);
   }
 });
