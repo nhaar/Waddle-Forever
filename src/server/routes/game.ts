36cfbf14a1a142b71def6e6a69231c1ed2e9ba5b
nhaar <38634785+nhaar@users.noreply.github.com>
2026-04-28 07:12:05 -0300

    remove long dead redirects and static servers

--- src/server/routes/game.ts
+++ src/server/routes/game.ts
@@ -84,16 +84,6 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
     req.send(getNewsTxt(settingsManager.settings.version));
   })
 
-  // important redirects
-  server.redirectDirs(
-    ['play/v2/content/global/clothing', 'clothing'], // clothing is its own package due to its high size
-    ['play/v2/content/global/music', 'default/music'], // (engine 2) non fundamental music that may potentially become a package,
-    ['music', 'default/static/engine2/play/v2/content/global/music'], // sharing the engine 1 and engine 2 music,
-    ['music', 'default/music'], // same as the two above
-    ['play/v2/content/global/furniture', 'default/furniture'], // furniture may become a package in the future
-    ['artwork/items', 'clothing/sprites'] // items in engine 1 (TODO try removing this, I thought items were in chat)
-  );
-
   // text file generating
 
   server.getData('load.swf', (s) => {
@@ -104,18 +94,6 @@ export function createHttpServer(settingsManager: SettingsManager): HttpServer {
       IP: s.targetIP,
       URLPRE: `http://${s.targetIP.slice(0, 3)}`
     });
-  });
-  
-  // FALL BACK AND STATIC SERVING
-  server.dir('', (s) => {
-    return 'static'
-  })
-  // SECOND LAYER FALLBACK
-  server.dir('', (s) => {
-    if (isEngine3(s.settings.version) || isEngine2(s.settings.version)) {
-      return 'default/static/engine2_3';
-    }
-  })
-  
+  });  
   return server
 }
