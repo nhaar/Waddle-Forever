fcb91b349ea3344ab6b90be1451bd5cfeebe6568
nhaar <38634785+nhaar@users.noreply.github.com>
2026-05-14 19:20:42 -0300

    base world room class without networking

--- src/server/socket-server/world/world-room.ts
+++ src/server/socket-server/world/world-room.ts
@@ -1,53 +1,36 @@
-import { PenguinEquipmentSlot } from "@server/penguin";
-import { FindFourTable } from "./find-four";
-import { MancalaTable } from "./mancala";
 import { WaddleRoom } from "./waddle-room";
-import { ContextAdder, ContextRemover, WorldEntity } from "./world-penguin";
+import { WorldEntity } from "./world-penguin";
 import { RoomState, WorldPenguin } from "./world-penguin";
 import { WorldTable } from "./world-table";
-
-/** Maps the slot name to what it's called in the packets */
-const EQUIP_SLOT_MAPPINGS: Record<PenguinEquipmentSlot, string> = {
-  color: 'c',
-  head: 'h',
-  face: 'f',
-  neck: 'n',
-  body: 'b',
-  hand: 'a',
-  feet: 'e',
-  pin: 'l',
-  background: 'p'
-}
 
 export class WorldRoom extends WorldEntity {
   private penguins = new Map<WorldPenguin, RoomState>();
   private waddles = new Map<number, WaddleRoom>();
   private tables = new Map<number, WorldTable>();
 
-  constructor(onAdd: (p: WorldPenguin, e: WorldEntity) => void, onRemove: (p: WorldPenguin) => void, private id: number) {
+  constructor(onAdd: (p: WorldPenguin, e: WorldEntity) => void, onRemove: (p: WorldPenguin) => void, private _id: number) {
     super(onAdd, onRemove);
   }
 
   public getPlayers(): string[] {
-    return [...this.penguins.entries()].map(([penguin, info]) => {
-      return penguin.getString(info);
-    });
+    return [];
   }
 
   public getPlayerStates() {
     return this.penguins.entries();
   }
 
-  public addPenguin(penguin: WorldPenguin, x: number, y: number): void {
-    this.addClient(penguin);
-    const state = { x, y, frame: 1 };
-    this.penguins.set(penguin, state);
+  public get players() {
+    return [...this.penguins.keys()];
+  }
 
-    const string = penguin.getString(state);
-    penguin.sendXt('jr', this.id, ...this.getPlayers());
-    this.sendXt('ap', string);
-    // it seems that the new x, y position of players must be sent via a new set position packet
-    this.move(penguin, x, y);
+  public get playerStates() {
+    return [...this.penguins.entries()];
+  }
+
+  public addPenguin(penguin: WorldPenguin, x: number, y: number): void {
+    this.penguins.set(penguin, { x, y, frame : 1});
+    this.addClient(penguin);
   }
 
   public getState(penguin: WorldPenguin) {
@@ -58,33 +41,9 @@ export class WorldRoom extends WorldEntity {
     return state;
   }
 
-  public move(penguin: WorldPenguin, x: number, y: number): void {
-    this.penguins.set(penguin, { x, y, frame: 1 });
-    this.sendXt('sp', penguin.id, x, y);
-  }
-
-  public teleport(penguin: WorldPenguin, x: number, y: number, frame: number): void {
-    this.penguins.set(penguin, { x, y, frame });
-    this.sendXt('st', penguin.id, x, y, frame);
-  }
-
   public removePenguin(penguin: WorldPenguin): void {
     this.removeClient(penguin);
     this.penguins.delete(penguin);
-
-    const players = this.getPlayers();
-    // because minigames get the player from their previous room, you can't
-    // send the remove player packet to the player leaving otherwise it won't
-    // find itself and minigame features (the penguin color) won't work
-    this.sendXt('rp', penguin.id, ...players);
-  }
-
-  public sendXt(message: string, ...args:Array<string | number>): void {
-    [...this.penguins.keys()].forEach(p => p.sendXt(message, ...args));
-  }
-
-  public throwSnowball(penguin: WorldPenguin, x: string, y: string): void {
-    this.sendXt('sb', penguin.id, x, y);
   }
 
   public addWaddle(id: number, waddle: WaddleRoom): void {
@@ -99,29 +58,30 @@ export class WorldRoom extends WorldEntity {
     return this.waddles.get(id);
   }
 
-  public enterWaddleRoom(waddle: WaddleRoom, penguin: WorldPenguin): void {
-    const seat = waddle.addPenguin(penguin)
-    penguin.sendXt('jw', seat);
-    this.sendXt('uw', waddle.getId(), seat, penguin.info.name, penguin.id);
+  public enterWaddleRoom(waddle: WaddleRoom, penguin: WorldPenguin): number {
+    return waddle.addPenguin(penguin)
   }
 
   public getTable(id: number) {
     let table = this.tables.get(id);
     if (table === undefined) {
-      if (WorldTable.FIND_FOUR_TABLE_IDS.has(id)) {
-        table = new FindFourTable(id);
-      } else if (WorldTable.MANCALA_TABLE_IDS.has(id)) {
-        table = new MancalaTable(id);
-      } else {
-        throw new Error('Unknown table id');
-      }
-
-      this.tables.set(id, table);
+      throw new Error();
     }
+    // if (table === undefined) {
+    //   if (WorldTable.FIND_FOUR_TABLE_IDS.has(id)) {
+    //     table = new FindFourTable(id);
+    //   } else if (WorldTable.MANCALA_TABLE_IDS.has(id)) {
+    //     table = new MancalaTable(id);
+    //   } else {
+    //     throw new Error('Unknown table id');
+    //   }
+
+    //   this.tables.set(id, table);
+    // }
     return table;
   }
 
-  public getPenguinTable(penguin: WorldPenguin): WorldTable | null {
+  public getPenguinTable(penguin: WorldPenguin) {
     for (const table of this.tables.values()) {
       if (table.hasPlayer(penguin)) {
         return table;
@@ -131,12 +91,8 @@ export class WorldRoom extends WorldEntity {
     return null;
   }
 
-  public sendTableState(table: WorldTable) {
-    this.sendXt('ut', table.getId(), table.getCount());
-  }
-
-  public getId() {
-    return this.id;
+  public get id() {
+    return this._id;
   }
 
   public setFrame(penguin: WorldPenguin, frame: number) {
@@ -146,8 +102,12 @@ export class WorldRoom extends WorldEntity {
     }
   }
 
-  public updateEquipment(penguin: WorldPenguin, slot: PenguinEquipmentSlot, id: number): void {
-    penguin.info[slot] = id;
-    this.sendXt(`up${EQUIP_SLOT_MAPPINGS[slot]}`, penguin.id, id);
+  public updatePosition(penguin: WorldPenguin, x: number, y: number): void {
+    // dance animation resets
+    this.penguins.set(penguin, { x, y, frame: 1 });
+  }
+
+  public updateFrame(penguin: WorldPenguin, frame: number): void {
+    this.penguins.set(penguin, { ...this.getState(penguin), frame });
   }
 }
