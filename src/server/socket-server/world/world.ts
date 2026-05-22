a686fda38a2b90bdad8bb451fd5777dae573dc37
nhaar <38634785+nhaar@users.noreply.github.com>
2026-05-22 07:40:48 -0300

    add basic world class

--- src/server/socket-server/world/world.ts
+++ src/server/socket-server/world/world.ts
@@ -1,65 +1,39 @@
 // ZA WAAAAARUDO!
 
 import { Vector } from "@common/utils";
-import { JsonDatabase } from "@server/database";
+import { PenguinRepository } from "@server/database/database";
 import { WaddleName, WADDLE_ROOMS } from "@server/game-logic/waddles";
-import { Penguin } from "@server/penguin";
+import { PenguinMessenger } from "@server/handlers/messenger";
 import { SettingsManager } from "@server/settings";
 import { GameData } from "@server/timelines/game-data";
 import { ClientSocket } from "..";
-import { Bakery } from "./bakery";
-import { CardJitsu } from "./card";
-import { CardJitsuFire } from "./fire";
-import { MatchMaker } from "./matchmaker";
 import { SledRace } from "./sled";
 import { WaddleGame } from "./waddle-game";
 import { WaddleRoom } from "./waddle-room";
-import { PenguinMessenger } from "./world-client";
 import { WorldGame } from "./world-game";
 import { WorldPenguin } from "./world-penguin";
 import { WorldRoom } from "./world-room";
-import { WorldTable } from "./world-table";
+
+type PenguinState = {
+  room?: WorldRoom;
+  game?: WorldGame;
+  sled?: SledRace;
+};
 
 export class World {
-  private clients = new Map<ClientSocket, ValidCtxObj<WorldContext>>();
   private penguins = new Map<number, WorldPenguin>();
+  private states = new Map<WorldPenguin, PenguinState>
   private rooms = new Map<number, WorldRoom>();
   private games = new Map<number, WorldGame>();
   private spectators = new Set<WorldPenguin>();
   private igloos = new Set<WorldPenguin>();
-  private bakery = new Bakery(this.getRoom(853));
   
-  // take this ugly thing away from my face at some point
-  private matchmaker = new MatchMaker(2, (players) => {
-    const game = this.getWaddleGame('card', players) as CardJitsu;
-    game.startMatch();
-  }, (players, time) => {
-    const nicknames = players.map(p => p.info.name);
-    players.forEach(p => p.sendXt('tmm', time, ...nicknames));
-  });
-
-  public get cardMatchmaker() {
-    return this.matchmaker;
-  }
-
   // create class responsible for the puck
   private _puckPosition = new Vector(0, 0);
   private _puckPositionParty = new Vector(0, 0);
 
-  constructor(private gameData: GameData, private settings: SettingsManager, private db: JsonDatabase) {
+  constructor(private gameData: GameData) {
     this.init();
-  }
-
-  public get data() {
-    return this.gameData;
-  }
-
-  public getSettings() {
-    return this.settings;
-  }
-
-  public getDb() {
-    return this.db;
   }
 
   public getRoom(id: number): WorldRoom {
@@ -71,53 +45,27 @@ export class World {
     return room;
   }
 
-  public addPenguin(messenger: PenguinMessenger, penguin: WorldPenguin): void {
-    const client = [...this.clients.entries()].find(([_, ctx]) => {
-      return ctx.messenger === messenger;
-    });
-    if (client !== undefined) {
-      this.penguins.set(penguin.id, penguin);
-      this.addContext(client[0], 'penguin', penguin);
-    }
+  public addPenguin(penguin: WorldPenguin): void {
+    this.penguins.set(penguin.id, penguin);
+    this.states.set(penguin, {});
   }
 
-  public getContext(client: ClientSocket): ValidCtxObj<WorldContext>;
-  public getContext(penguin: WorldPenguin): ValidCtxObj<WorldContext>;
-
-  public getContext(arg: ClientSocket | WorldPenguin): ValidCtxObj<WorldContext> {
-    if (arg instanceof WorldPenguin) {
-      const ctx = [...this.clients.values()].find((ctx) => ctx.penguin === arg);
-      if (ctx === undefined) {
-        throw new Error('Penguin has no context');
-      }
-      return ctx;
-    }
-    let ctx = this.clients.get(arg);
-    if (ctx !== undefined) {
-      return ctx;
-    }
-    const messenger = new PenguinMessenger(arg);
-    ctx = { messenger };
-    this.clients.set(arg, ctx);
-    return ctx;
+  public getContext(p: WorldPenguin): PenguinState | undefined {
+    return this.states.get(p);
   }
 
-  private addContext<T extends keyof WorldContext & string>(client: ClientSocket, name: T, entity: WorldContext[T]): void;
-  private addContext<T extends keyof WorldContext & string>(penguin: WorldPenguin, name: T, entity: WorldContext[T]): void;
-
-  private addContext<T extends keyof WorldContext & string>(target: ClientSocket | WorldPenguin, name: T, entity: WorldContext[T]) {
-    let ctx;
-    if (target instanceof WorldPenguin) {
-      ctx = this.getContext(target);
-    } else {
-      ctx = this.getContext(target);
-    }
-    ctx[name] = entity;
-  }
-
-  private removeContext<T extends keyof WorldContext & string>(penguin: WorldPenguin, name: T): void {
+  private addContext<T extends keyof PenguinState>(penguin: WorldPenguin, name: T, entity: PenguinState[T]): void {
     const ctx = this.getContext(penguin);
-    ctx[name] = undefined;
+    if (ctx !== undefined) {
+      ctx[name] = entity;
+    }
+  }
+
+  private removeContext<T extends keyof PenguinState>(penguin: WorldPenguin, name: T): void {
+    const ctx = this.getContext(penguin);
+    if (ctx !== undefined) {
+      ctx[name] = undefined;
+    }
   }
 
   public getPenguin(id: number): WorldPenguin | undefined {
@@ -133,57 +81,7 @@ export class World {
     return game;
   }
 
-  public disconnect(client: ClientSocket): void {
-
-
-
-    const context = this.getContext(client) ?? {};
-    const { room, penguin } = context;
-    if (room !== undefined && penguin !== undefined) {
-      room.removePenguin(penguin);
-    }
-    if (penguin !== undefined) {
-      if (penguin.isAgentPending()) {
-        penguin.addItem(800);
-      }
-      if (room !== undefined) {
-        const table = room.getPenguinTable(penguin);
-        if (table !== null) {
-          const seat = table.getSeatIndex(penguin);
-          if (seat !== WorldTable.TABLE_SPECTATOR_SEAT && seat !== undefined) {
-            if (table.hasStarted() && table.hasJoined(seat)) {
-              table.sendXt('cz', penguin.info.name);
-              table.resetRound();
-              room.sendTableState(table);
-            } else {
-              table.removePlayer(penguin);
-              const count = table.getCount();
-              room.sendTableState(table);
-              if (count === 0) {
-                table.reset();
-              }
-            }
-          }
-          
-        }
-      }
-  
-    penguin.info.getBuddies().forEach((buddyId) => {
-      const buddyClient = this.getPenguin(buddyId);
-      if (buddyClient !== undefined) {
-        this.sendBuddyOnlineList(buddyClient, penguin.id);
-      }
-    });
-
-      const delta = Date.now() - penguin.sessionStart;
-      const minutesDelta = delta / 1000 / 60;
-      penguin.info.incrementPlayTime(minutesDelta);
-      penguin.info.update();
-      this.penguins.delete(penguin.id);
-    }
-
-    this.clients.delete(client);
-    client.end();
+  public disconnect(penguin: WorldPenguin): void {
   }
 
   public init() {
@@ -192,79 +90,34 @@ export class World {
       const room = this.getRoom(waddle.roomId);
       room.addWaddle(waddle.waddleId, new WaddleRoom(waddle.waddleId, waddle.seats, waddle.game));
     });
-
-    // TODO this
-    this._puckPosition = new Vector(0, 0);
-    this._puckPositionParty = new Vector(0, 0);
   }
 
   public getWaddleGame(name: WaddleName, players: WorldPenguin[]): WaddleGame {
     let game: WaddleGame;
 
     switch (name) {
-      case 'card':
-        game = new CardJitsu(players, (c, e) => this.addContext(c, 'card', e as CardJitsu), (c) => this.removeContext(c, 'card'));
-        break;
-      case 'fire':
-        game = new CardJitsuFire(players, (c, e) => this.addContext(c, 'fire', e as CardJitsuFire), (c) => this.removeContext(c, 'fire'));
-        break;
+    //   case 'card':
+    //     game = new CardJitsu(players, (c, e) => this.addContext(c, 'card', e as CardJitsu), (c) => this.removeContext(c, 'card'));
+    //     break;
+    //   case 'fire':
+    //     game = new CardJitsuFire(players, (c, e) => this.addContext(c, 'fire', e as CardJitsuFire), (c) => this.removeContext(c, 'fire'));
+    //     break;
       case 'sled':
         game = new SledRace(players, (c, e) => this.addContext(c, 'sled', e as SledRace), (c) => this.removeContext(c, 'sled'));
         break;
       default:
         throw new Error('No waddle game constructor set');
     }
-
+    
     return game;
   }
 
-  public formatBuddyEntry(id: number, includeOnlineFlag: boolean): string {
-    const name = Penguin.getById(id)?.name ?? this.penguins.get(id)?.info.name ?? 'Unknown';
-    if (!includeOnlineFlag) {
-      return `${id}|${name}`;
-    }
-    const online = this.penguins.get(id) !== undefined;
-    return online ? `${id}|${name}|1` : `${id}|${name}`;
-  }
-
-  public handleGetBuddies(penguin: WorldPenguin) {
-    // TODO currently pre-cpip only
-    if (!this.gameData.isPreCpip()) {
-      return;
-    }
-    const buddies = penguin.info.getBuddies()
-      .map((id) => this.formatBuddyEntry(id, true));
-    if (buddies.length === 0) {
-      penguin.sendXtEmptyLast('gb');
-      return;
-    }
-    penguin.sendXt('gb', ...buddies);
+  public getById(id: number) {
+    return this.penguins.get(id);
   }
 
   public removeSpectator(penguin: WorldPenguin) {
     return this.spectators.delete(penguin);
-  }
-
-  public handleGetBuddyOnlineList(penguin: WorldPenguin) {
-    if (!this.gameData.isPreCpip()) {
-      return;
-    }
-    const onlineIds = penguin.info.getBuddies().filter((id) => this.penguins.get(id) !== undefined);
-    if (onlineIds.length === 0) {
-      penguin.sendXtEmptyLast('go');
-      return;
-    }
-    penguin.sendXt('go', ...onlineIds);
-  };
-
-  public sendBuddyOnlineList(penguin: WorldPenguin, excludeId?: number): void {
-    const onlineIds = penguin.info.getBuddies().filter((id) => {
-      if (excludeId !== undefined && id === excludeId) {
-        return false;
-      }
-      return this.penguins.get(id) !== undefined;
-    });
-    penguin.sendXt('go', ...onlineIds);
   }
 
   // maybe a class for managing open igloos 
@@ -280,10 +133,6 @@ export class World {
     return [...this.igloos.values()];
   }
 
-  public getBakery() {
-    return this.bakery;
-  }
-
   public getPuckPosition() {
     return this._puckPosition.vector;
   }
@@ -291,15 +140,26 @@ export class World {
   public getPuckPositionParty() {
     return this._puckPositionParty.vector;
   }
+
+  public get players() {
+    return [...this.penguins.values()];
+  }
 }
+
+export type PenguinPersister = (p: WorldPenguin, force?: boolean) => void;
 
 export interface WorldContext {
   'world': World;
   'penguin': WorldPenguin;
+  'msg': PenguinMessenger;
   'room': WorldRoom;
   'game': WorldGame;
-  'card': CardJitsu;
-  'fire': CardJitsuFire;
+  'data': GameData;
+  'client': ClientSocket;
+  'settings': SettingsManager;
+  'db': PenguinRepository;
+  'prst': PenguinPersister;
+  // 'card': CardJitsu;
+  // 'fire': CardJitsuFire;
   'sled': SledRace;
-  'messenger': PenguinMessenger
 }
