d22cd70105575623cb85f0fbf4b6dbddf8d9f535
nhaar <38634785+nhaar@users.noreply.github.com>
2026-05-22 07:42:10 -0300

    add basic waddle room

--- src/server/socket-server/world/waddle-room.ts
+++ src/server/socket-server/world/waddle-room.ts
@@ -2,11 +2,13 @@ import { WaddleName } from "@server/game-logic/waddles";
 import { WorldPenguin } from "./world-penguin";
 
 export class WaddleRoom {
-  private players: Array<WorldPenguin | null>;
-  private seatIndex = 0;
+  private _seats: Array<WorldPenguin | null>;
+  private _capacity: number;
+  private _takenIndex = 0;
 
-  constructor(private id: number, private seats: number, private game: WaddleName) {
-    this.players = new Array(seats).fill(null);
+  constructor(private id: number, seats: number, private game: WaddleName) {
+    this._capacity = seats;
+    this._seats = new Array(seats).fill(null);
   }
 
   public getId() {
@@ -14,18 +16,25 @@ export class WaddleRoom {
   }
 
   public getSeats() {
-    return [...this.players];
+    return [...this._seats];
   }
 
-  public addPenguin(penguin: WorldPenguin): number {
-    this.players[this.seatIndex] = penguin;
-    const seat = this.seatIndex;
-    this.seatIndex++;
-    return seat;
+  public addPenguin(penguin: WorldPenguin): number | null {
+    if (this._takenIndex < this._capacity) {
+      const seat = this._takenIndex;
+      this._seats[seat] = penguin;
+      for (this._takenIndex = seat + 1; this._takenIndex < this._capacity; this._takenIndex++) {
+        if (this._seats[this._takenIndex] === null) {
+          break;
+        }
+      }
+      return seat;
+    }
+    return null;
   }
 
   public isFull() {
-    return this.seatIndex === this.players.length;
+    return this._takenIndex >= this._capacity;
   }
 
   public getGame() {
@@ -33,7 +42,21 @@ export class WaddleRoom {
   }
 
   public reset() {
-    this.players = new Array(this.seats).fill(null);
-    return this.seatIndex = 0;
+    this._seats = new Array(this._capacity).fill(null);
+    this._takenIndex = 0;
+  }
+
+  public removePlayer(penguin: WorldPenguin) {
+    const index = this._seats.findIndex(p => p === penguin);
+    if (index > -1) {
+      this._seats[index] = null;
+      for (let i = 0; i < this._capacity; i++) {
+        if (this._seats[i] === null) {
+          this._takenIndex = i;
+          break;
+        }
+      }
+    }
+    return index;
   }
 }
