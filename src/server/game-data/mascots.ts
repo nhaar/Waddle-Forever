603af3a7040f81323a74ee5674c13a70107a31d4
zaid-mahdi <zayoodi100@gmail.com>
2026-08-03 01:32:31 +0300

    Fixed messing mascot items from db

--- src/server/game-data/mascots.ts
+++ src/server/game-data/mascots.ts
@@ -3,13 +3,55 @@ type Mascot = {
   name: string;
   display?: string;
   starterItems: number[];
-}
+};
 
 export const MASCOTS: Mascot[] = [
   {
     id: 10,
-    name: 'Herbert',
-    display: 'Herbert P Bear',
-    starterItems: []
-  }
-]
+    name: "Herbert",
+    display: "Herbert P Bear",
+    starterItems: [],
+  },
+  {
+    id: 117,
+    name: "Dott",
+    display: "Dot",
+    starterItems: [],
+  },
+  {
+    id: 119,
+    name: "Puffle H",
+    display: "PH",
+    starterItems: [],
+  },
+  {
+    id: 121,
+    name: "Samm",
+    display: "Sam",
+    starterItems: [],
+  },
+  {
+    id: 20,
+    name: "Merry Walrus",
+    display: "Merry Walrus",
+    starterItems: [1964],
+  },
+  {
+    id: 131,
+    name: "Cece",
+    display: "CeCe",
+    starterItems: [],
+  },
+  {
+    id: 27,
+    name: "Billybob",
+    display: "Billybob",
+    starterItems: [1682, 1683],
+  },
+  {
+    id: 9,
+    name: "Sensei",
+    display: "Sensei",
+    starterItems: [],
+  },
+];
