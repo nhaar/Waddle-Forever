07c4179c26c4a7d5670c1a6a60ca239aa6bd0a99
Tristan Verduzco <64928177+VerduzcoTristan@users.noreply.github.com>
2026-08-06 14:42:37 -0500

    Fix Find Four and Mancala startup

--- src/server/socket-server/world/world-table.ts
+++ src/server/socket-server/world/world-table.ts
@@ -71,7 +71,7 @@ export abstract class WorldTable {
   }
 
   public hasPlayer(penguin: WorldPenguin) {
-    return this.getSeatIndex(penguin) !== -1;
+    return this.getSeatIndex(penguin) !== undefined;
   }
 
   public removePlayer(penguin: WorldPenguin) {
