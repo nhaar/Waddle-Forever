51f8d13e8d6af45476c5d85f55696aef7186434f
Zaid Mahdi <85694548+zaid-mahdi@users.noreply.github.com>
2026-08-14 15:29:10 +0300

    Add missing mascot items from db (#198)
    * Fixed messing mascot items from db
    
    * minify mascots.json
    
    * fix incorrect mascot ids
    
    * archive files: 115, 1271, 1458
    
    * archives for 1459, 2033
    
    * add archives 3130, 3131, 3480
    
    * add archives 4555, 4690, 4691
    
    * add some archives files
    
    * add archive files
    
    * remove files
    
    * remove unexisting files
    
    * cleanup
    
    ---------
    
    Co-authored-by: nhaar <38634785+nhaar@users.noreply.github.com>

--- src/server/game-data/mascots.ts
+++ src/server/game-data/mascots.ts
@@ -3,13 +3,49 @@ type Mascot = {
   name: string;
   display?: string;
   starterItems: number[];
-}
+};
 
 export const MASCOTS: Mascot[] = [
   {
     id: 10,
-    name: 'Herbert',
-    display: 'Herbert P Bear',
-    starterItems: []
-  }
-]
+    name: "Herbert",
+    display: "Herbert P Bear",
+    starterItems: [],
+  },
+  {
+    id: 15,
+    name: "Dott",
+    display: "Dot",
+    starterItems: [],
+  },
+  {
+    id: 21,
+    name: "Puffle H",
+    display: "PH",
+    starterItems: [],
+  },
+  {
+    id: 25,
+    name: "Samm",
+    display: "Sam",
+    starterItems: [],
+  },
+  {
+    id: 36,
+    name: "Merry Walrus",
+    display: "Merry Walrus",
+    starterItems: [1964],
+  },
+  {
+    id: 27,
+    name: "Cece",
+    display: "CeCe",
+    starterItems: [],
+  },
+  {
+    id: 9,
+    name: "Sensei",
+    display: "Sensei",
+    starterItems: [],
+  },
+];
