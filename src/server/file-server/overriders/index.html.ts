d21b8c0d0dde365c8c700bc90c24e4aeb5c985d8
ChrisCPI <92183829+ChrisCPI@users.noreply.github.com>
2026-09-10 17:25:54 -0400

    Inject media url for modern-as3.html

--- src/server/file-server/overriders/index.html.ts
+++ src/server/file-server/overriders/index.html.ts
@@ -2,33 +2,6 @@ import { getMediaFile } from "@server/game-data/files";
 import { SettingsManager } from "@server/settings";
 import { GameData } from "@server/timelines/game-data";
 
-function injectRuffleIntoHtml(html: string, ip: string, loginPort: number, worldPort: number) {
-  const socketProxy = JSON.stringify([
-    {
-      host: ip,
-      port: loginPort,
-      proxyUrl: `ws://${ip}:${loginPort}`,
-    },
-    {
-      host: ip,
-      port: worldPort,
-      proxyUrl: `ws://${ip}:${worldPort}`,
-    },
-  ]);
-
-  const injectedScript = `
-    <script>
-      window.RufflePlayer = window.RufflePlayer || {};
-      window.RufflePlayer.config = {
-        ...window.RufflePlayer.config,
-        socketProxy: ${socketProxy}
-      };
-    </script>
-  `;
-
-  return html.replace('</head>', `${injectedScript}</head>`);
-}
-
 export async function overrideIndexHtml(d: GameData, s: SettingsManager, b: Buffer | string): Promise<Buffer | string> {
   let newFileRef: string | null = null;
 
@@ -52,5 +25,32 @@ export async function overrideIndexHtml(d: GameData, s: SettingsManager, b: Buff
     b = b.toString();
   }
 
-  return injectRuffleIntoHtml(b, s.targetIP, s.loginPort, s.worldPort);
+  // For modern-as3.html, inject the correct url for media
+  b = b.replaceAll('##MEDIA_URL##', `http://${s.targetIP}:${s.targetPort}/`);
+
+  // Ruffle socket proxy
+  const socketProxy = JSON.stringify([
+    {
+      host: s.targetIP,
+      port: s.loginPort,
+      proxyUrl: `ws://${s.targetIP}:${s.loginPort}`,
+    },
+    {
+      host: s.targetIP,
+      port: s.worldPort,
+      proxyUrl: `ws://${s.targetIP}:${s.worldPort}`,
+    },
+  ]);
+
+  const injectedScript = `
+    <script>
+      window.RufflePlayer = window.RufflePlayer || {};
+      window.RufflePlayer.config = {
+        ...window.RufflePlayer.config,
+        socketProxy: ${socketProxy}
+      };
+    </script>
+  `;
+
+  return b.replace('</head>', `${injectedScript}</head>`);
 }
